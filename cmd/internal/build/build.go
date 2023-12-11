a1211a98e380877d5e8e641c61e50b1bd7105378
xushiwei <x@goplus.org>
2023-12-11 09:53:40 +0800

    cmd/llgo

---
+++ cmd/internal/build/build.go
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2023 The GoPlus Authors (goplus.org). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Package build implements the “llgo build” command.
+package build
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"reflect"
+
+	"github.com/goplus/gop"
+	"github.com/goplus/gop/x/gopprojs"
+	"github.com/goplus/llgo"
+	"github.com/goplus/llgo/cmd/internal/base"
+	"github.com/goplus/llgo/x/gocmd"
+)
+
+// llgo build
+var Cmd = &base.Command{
+	UsageLine: "llog build [flags] [packages]",
+	Short:     "Build Go files",
+}
+
+var (
+	flagOutput = flag.String("o", "", "build output file")
+	_          = flag.Bool("v", false, "print verbose information")
+	flag       = &Cmd.Flag
+)
+
+func init() {
+	Cmd.Run = runCmd
+}
+
+func runCmd(cmd *base.Command, args []string) {
+	err := flag.Parse(args)
+	if err != nil {
+		log.Panicln("parse input arguments failed:", err)
+	}
+
+	args = flag.Args()
+	if len(args) == 0 {
+		args = []string{"."}
+	}
+
+	proj, args, err := gopprojs.ParseOne(args...)
+	if err != nil {
+		log.Panicln(err)
+	}
+	if len(args) != 0 {
+		log.Panicln("too many arguments:", args)
+	}
+
+	conf := &llgo.Config{}
+	confCmd := &gocmd.BuildConfig{}
+	if *flagOutput != "" {
+		output, err := filepath.Abs(*flagOutput)
+		if err != nil {
+			log.Panicln(err)
+		}
+		confCmd.Output = output
+	}
+	build(proj, conf, confCmd)
+}
+
+func build(proj gopprojs.Proj, conf *llgo.Config, build *gocmd.BuildConfig) {
+	var obj string
+	var err error
+	switch v := proj.(type) {
+	case *gopprojs.DirProj:
+		obj = v.Dir
+		err = llgo.BuildDir(obj, conf, build)
+	case *gopprojs.PkgPathProj:
+		obj = v.Path
+		err = llgo.BuildPkgPath("", v.Path, conf, build)
+	case *gopprojs.FilesProj:
+		err = llgo.BuildFiles(v.Files, conf, build)
+	default:
+		log.Panicln("`llgo build` doesn't support", reflect.TypeOf(v))
+	}
+	if gop.NotFound(err) {
+		fmt.Fprintf(os.Stderr, "llgo build %v: not found\n", obj)
+	} else if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	} else {
+		return
+	}
+	os.Exit(1)
+}
+
+// -----------------------------------------------------------------------------
--- go.mod
+++ go.mod
@@ -5,7 +5,14 @@ go 1.18
 require (
 	github.com/aykevl/go-wasm v0.0.1
 	github.com/blakesmith/ar v0.0.0-20190502131153-809d4375e1fb
+	github.com/goplus/gop v1.1.13
 	github.com/qiniu/x v1.13.1
 	golang.org/x/tools v0.16.0
 	tinygo.org/x/go-llvm v0.0.0-20231014233752-75a8a9fe6f74
+)
+
+require (
+	github.com/goplus/gox v1.13.1 // indirect
+	github.com/goplus/mod v0.11.9 // indirect
+	golang.org/x/mod v0.14.0 // indirect
 )
