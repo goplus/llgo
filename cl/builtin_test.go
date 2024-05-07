073cac8530ac87fa7d4d7633ba7516baac670bd6
xushiwei <x@goplus.org>
2024-05-07 17:39:12 +0800

    TestErrInitLinkname

--- cl/builtin_test.go
+++ cl/builtin_test.go
@@ -159,12 +159,15 @@ func TestErrImport(t *testing.T) {
 }
 
 func TestErrInitLinkname(t *testing.T) {
+	var ctx context
+	ctx.initLinkname("foo", "//llgo:link abc", func(name string) (bool, bool) {
+		return false, false
+	})
 	defer func() {
 		if r := recover(); r == nil {
 			t.Fatal("initLinkname: no error?")
 		}
 	}()
-	var ctx context
 	ctx.initLinkname("foo", "//go:linkname Printf printf", func(name string) (isVar bool, ok bool) {
 		return false, name == "Printf"
 	})
