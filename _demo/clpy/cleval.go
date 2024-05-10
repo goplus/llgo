d9db5528f527af43552e97c85f376d4cac4180ba
xushiwei <x@goplus.org>
2024-05-11 04:26:41 +0800

    py demo: hellpy, clpy, callpy

---
+++ _demo/clpy/cleval.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/goplus/llgo/c"
+	"github.com/goplus/llgo/py"
+)
+
+func main() {
+	py.Initialize()
+	py.SetProgramName(*c.Argv)
+	code := py.CompileString(c.Str(`print('Hello, World!')`), c.Str(`hello.py`), py.EvalInput)
+	if code != nil {
+		mod := py.ImportModule(c.Str("__main__"))
+		gbl := mod.GetDict()
+
+		result := py.EvalCode(code, gbl, nil)
+
+		result.DecRef()
+		mod.DecRef()
+		code.DecRef()
+	}
+	py.Finalize()
+}
