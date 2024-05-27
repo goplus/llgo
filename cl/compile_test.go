a6f92b8ff99d6a35ba135f07e6ddda9310da5ad4
xushiwei <x@goplus.org>
2024-05-27 11:28:03 +0800

    TestFromTestgo

--- cl/compile_test.go
+++ cl/compile_test.go
@@ -28,6 +28,10 @@ func testCompile(t *testing.T, src, expected string) {
 	cltest.TestCompileEx(t, src, "foo.go", expected)
 }
 
+func TestFromTestgo(t *testing.T) {
+	cltest.FromDir(t, "", "./_testgo", false)
+}
+
 func TestFromTestpy(t *testing.T) {
 	cltest.FromDir(t, "", "./_testpy", false)
 }
