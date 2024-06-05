519e69a7f81b0ff1db1eb9bbbfd0207674c3e5fc
xushiwei <x@goplus.org>
2024-06-05 13:48:11 +0800

    cl/blocks

---
+++ cl/_testdefer/print/in.go
@@ -0,0 +1,52 @@
+package main
+
+func f() float64 {
+	return 1.0
+}
+
+func main() {
+	var v = f()
+	const n = 7 // digits printed
+	var buf [n + 7]byte
+	buf[0] = '+'
+	e := 0 // exp
+	if v == 0 {
+		if 1/v < 0 {
+			buf[0] = '-'
+		}
+	} else {
+		if v < 0 {
+			v = -v
+			buf[0] = '-'
+		}
+
+		// normalize
+		for v >= 10 {
+			e++
+			v /= 10
+		}
+		for v < 1 {
+			e--
+			v *= 10
+		}
+
+		// round
+		h := 5.0
+		for i := 0; i < n; i++ {
+			h /= 10
+		}
+		v += h
+		if v >= 10 {
+			e++
+			v /= 10
+		}
+	}
+
+	// format +d.dddd+edd
+	for i := 0; i < n; i++ {
+		s := int(v)
+		buf[i+2] = byte(s + '0')
+		v -= float64(s)
+		v *= 10
+	}
+}
---
+++ cl/blocks/block.go
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c) 2024 The GoPlus Authors (goplus.org). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package blocks
+
+import (
+	llssa "github.com/goplus/llgo/ssa"
+	"golang.org/x/tools/go/ssa"
+)
+
+type Info struct {
+	Kind llssa.DoAction
+	Next int
+}
+
+// -----------------------------------------------------------------------------
+
+type blockState struct {
+	self   *ssa.BasicBlock
+	preds  int
+	succs  []int
+	loop   bool
+	always bool
+	fdel   bool
+}
+
+func (p *blockState) kind() llssa.DoAction {
+	if p.loop {
+		return llssa.DeferInLoop
+	}
+	if p.always {
+		return llssa.DeferAlways
+	}
+	return llssa.DeferInCond
+}
+
+func newSuccs(succs []*ssa.BasicBlock) []int {
+	ret := make([]int, len(succs))
+	for i, blk := range succs {
+		ret[i] = blk.Index
+	}
+	return ret
+}
+
+func findLoop(states []*blockState, path []int, from, iblk int) []int {
+	path = append(path, iblk)
+	self := states[iblk]
+	for _, succ := range self.succs {
+		if states[succ].fdel {
+			continue
+		}
+		if succ == from {
+			for _, i := range path {
+				states[i].loop = true
+			}
+			return path
+		}
+		if ret := findLoop(states, path, from, succ); ret != nil {
+			return ret
+		}
+	}
+	return nil
+}
+
+// https://en.wikipedia.org/wiki/Topological_sorting
+func Infos(blks []*ssa.BasicBlock) []Info {
+	n := len(blks)
+	order := make([]int, 1, n+1)
+	order[0] = 0
+	end, iend := 0, 0
+	states := make([]*blockState, n)
+	for i, blk := range blks {
+		preds := len(blk.Preds)
+		if preds == 0 && i != 0 {
+			order = append(order, i)
+		}
+		if isEnd(blk) {
+			end++
+			iend = i
+		}
+		states[i] = &blockState{
+			self:  blk,
+			preds: preds,
+			succs: newSuccs(blk.Succs),
+		}
+	}
+
+	path := make([]int, 0, n)
+	if states[0].preds != 0 {
+		if loop := findLoop(states, path, 0, 0); len(loop) > 0 {
+			order = append(order, loop[1:]...)
+		}
+	} else {
+		states[0].always = true
+	}
+	if end == 1 {
+		states[iend].always = true
+	}
+	pos := 0
+
+retry:
+	for pos < len(order) {
+		iblk := order[pos]
+		pos++
+		state := states[iblk]
+		state.fdel = true
+		for _, succ := range state.succs {
+			s := states[succ]
+			if s.fdel {
+				continue
+			}
+			if s.preds--; s.preds == 0 {
+				order = append(order, succ)
+			}
+		}
+	}
+	if pos < n {
+		for iblk, state := range states {
+			if state.fdel {
+				continue
+			}
+			if loop := findLoop(states, path, iblk, iblk); len(loop) > 0 {
+				order = append(order, loop...)
+				goto retry
+			}
+		}
+		panic("unreachable")
+	}
+	order = append(order, -1)
+	ret := make([]Info, n)
+	for i := 0; i < n; i++ {
+		iblk := order[i]
+		ret[iblk] = Info{states[iblk].kind(), order[i+1]}
+	}
+	return ret
+}
+
+func isEnd(blk *ssa.BasicBlock) bool {
+	// Note: skip recover block
+	return len(blk.Succs) == 0 && (len(blk.Preds) > 0 || blk.Index == 0)
+}
+
+// -----------------------------------------------------------------------------
