88ec34d09131b7d80847813e7f43faf3e1daef79
morpingsss <105426884+morpingsss@users.noreply.github.com>
2024-07-08 18:06:48 +0800

    Update inih.go

--- c/inih/inih.go
+++ c/inih/inih.go
@@ -22,8 +22,5 @@ func Parse(filename *c.Char, handler func(user c.Pointer, section *c.Char, name 
 //go:linkname ParseFile C.ini_parse_file
 func ParseFile(file c.FilePtr, handler func(user c.Pointer, section *c.Char, name *c.Char, value *c.Char) c.Int, user *c.Pointer) c.Int
 
-//go:linkname ParseStream C.ini_parse_stream
-func ParseStream(reader func(str *c.Char, num c.Int, stream c.Pointer) *c.Char, stream c.Pointer, handler func(user c.Pointer, section *c.Char, name *c.Char, value *c.Char) c.Int, user *c.Pointer) c.Int
-
 //go:linkname ParseString C.ini_parse_string
 func ParseString(str *c.Char, handler func(user c.Pointer, section *c.Char, name *c.Char, value *c.Char) c.Int, user *c.Pointer) c.Int
