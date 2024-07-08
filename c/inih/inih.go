c3033a3992fa7d15d1baa219cafd3189920102c8
morpingsss <105426884+morpingsss@users.noreply.github.com>
2024-07-08 18:22:04 +0800

    Update inih.go - 2

--- c/inih/inih.go
+++ c/inih/inih.go
@@ -15,10 +15,10 @@ const (
 )
 
 //go:linkname Parse C.ini_parse
-func Parse(filename *c.Char, handler func(user c.Pointer, section *c.Char, name *c.Char, value *c.Char) c.Int, user *c.Pointer) c.Int
+func Parse(filename *c.Char, handler func(user c.Pointer, section *c.Char, name *c.Char, value *c.Char) c.Int, user c.Pointer) c.Int
 
 //go:linkname ParseFile C.ini_parse_file
-func ParseFile(file c.FilePtr, handler func(user c.Pointer, section *c.Char, name *c.Char, value *c.Char) c.Int, user *c.Pointer) c.Int
+func ParseFile(file c.FilePtr, handler func(user c.Pointer, section *c.Char, name *c.Char, value *c.Char) c.Int, user c.Pointer) c.Int
 
 //go:linkname ParseString C.ini_parse_string
-func ParseString(str *c.Char, handler func(user c.Pointer, section *c.Char, name *c.Char, value *c.Char) c.Int, user *c.Pointer) c.Int
+func ParseString(str *c.Char, handler func(user c.Pointer, section *c.Char, name *c.Char, value *c.Char) c.Int, user c.Pointer) c.Int
