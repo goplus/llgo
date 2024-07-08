b6ca1293aa427e6567a057b9c61b6b905511dc31
morpingsss <morpingsss@outlook.com>
2024-07-08 17:36:56 +0800

    feat(llgo/c):Add an INI parser

--- c/inih/_demo/inihHello.go
+++ c/inih/_demo/inihHello.go
@@ -6,10 +6,8 @@ import (
 )
 
 func main() {
-	// 将Go字符串转换为C字符串
 	filename := c.Str("config.ini")
 
-	// 调用inih包的IniParse函数，解析INI文件
 	if inih.IniParse(filename, func(user c.Pointer, section *c.Char, name *c.Char, value *c.Char) c.Int {
 		println("section:", c.GoString(section), "name:", c.GoString(name), "value:", c.GoString(value))
 		return 1
@@ -18,7 +16,6 @@ func main() {
 		return
 	}
 
-	//  从文件中解析,流中解析
 	file := c.Fopen(c.Str("config.ini"), c.Str("r"))
 	if file == nil {
 		println("Error opening config file")
@@ -34,7 +31,6 @@ func main() {
 		c.Fclose(file)
 	}
 
-	// 从字符串中解析
 	configData := "[settings]\nusername=lee\ntimeout=20"
 	if inih.IniParseString(c.Str(configData), func(user c.Pointer, section *c.Char, name *c.Char, value *c.Char) c.Int {
 		println("section:", c.GoString(section), "name:", c.GoString(name), "value:", c.GoString(value))
