35ea80abdcf4afbf8d2792bdfb3a253dc10cb054
morpingsss <morpingsss@outlook.com>
2024-07-08 18:02:14 +0800

    feat(llgo/c):Add an INI parser

--- c/inih/inih.go
+++ c/inih/inih.go
@@ -16,14 +16,14 @@ const (
 	LLGoPackage = "link: $(pkg-config --libs inih); -linih"
 )
 
-//go:linkname IniParse C.ini_parse
-func IniParse(filename *c.Char, handler func(user c.Pointer, section *c.Char, name *c.Char, value *c.Char) c.Int, user *c.Pointer) c.Int
+//go:linkname Parse C.ini_parse
+func Parse(filename *c.Char, handler func(user c.Pointer, section *c.Char, name *c.Char, value *c.Char) c.Int, user *c.Pointer) c.Int
 
-//go:linkname IniParseFile C.ini_parse_file
-func IniParseFile(file c.FilePtr, handler func(user c.Pointer, section *c.Char, name *c.Char, value *c.Char) c.Int, user *c.Pointer) c.Int
+//go:linkname ParseFile C.ini_parse_file
+func ParseFile(file c.FilePtr, handler func(user c.Pointer, section *c.Char, name *c.Char, value *c.Char) c.Int, user *c.Pointer) c.Int
 
-//go:linkname IniParseStream C.ini_parse_stream
-func IniParseStream(reader func(str *c.Char, num c.Int, stream c.Pointer) *c.Char, stream c.Pointer, handler func(user c.Pointer, section *c.Char, name *c.Char, value *c.Char) c.Int, user *c.Pointer) c.Int
+//go:linkname ParseStream C.ini_parse_stream
+func ParseStream(reader func(str *c.Char, num c.Int, stream c.Pointer) *c.Char, stream c.Pointer, handler func(user c.Pointer, section *c.Char, name *c.Char, value *c.Char) c.Int, user *c.Pointer) c.Int
 
-//go:linkname IniParseString C.ini_parse_string
-func IniParseString(str *c.Char, handler func(user c.Pointer, section *c.Char, name *c.Char, value *c.Char) c.Int, user *c.Pointer) c.Int
+//go:linkname ParseString C.ini_parse_string
+func ParseString(str *c.Char, handler func(user c.Pointer, section *c.Char, name *c.Char, value *c.Char) c.Int, user *c.Pointer) c.Int
