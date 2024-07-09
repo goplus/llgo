1b024ec0888ca08571dc020ae3140685f202c5c7
2458zhanghehe <2458050004@qq.com>
2024-07-09 19:32:54 +0800

    feat(llgo/c/spdlog): Added a C++ logging library : spdlog

--- c/spdlog/cppWrap/cppWrap.cpp
+++ c/spdlog/cppWrap/cppWrap.cpp
@@ -1,14 +1,5 @@
 #include "spdlog/spdlog.h"
 
-// void PrintInfo(const char* msg);
-
-// int main() 
-// {
-//     spdlog::info("Welcome to spdlog!");
-//     PrintInfo("Hello from C++!");
-// }
-
-
  extern "C" void PrintInfo(const char* msg){
     spdlog::info(msg);
 }
