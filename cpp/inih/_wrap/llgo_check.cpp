767a0cc1fde72391d801afe900e2bd5b5678a528
xushiwei <x@goplus.org>
2024-07-13 19:53:55 +0800

    _wrap: llgo_check (to be continued)

---
+++ cpp/inih/_wrap/llgo_check.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <INIReader.h>
+
+int main() {
+    printf("sizeof(Reader) = %lu\n", sizeof(INIReader));
+    return 0;
+}
--- cpp/std/_wrap/string.cpp
+++ cpp/std/_wrap/string.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <new>
 
 extern "C" {
 
