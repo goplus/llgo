c22427b8fda2cc634a7ccd3602db5b7193970262
赵英杰 <spongehah@MacBook.local>
2024-07-15 10:19:55 +0800

    llgo/c/fcntl

--- _demo/fcntl/fcntl.go
+++ _demo/fcntl/fcntl.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/goplus/llgo/c"
-	"github.com/goplus/llgo/c/fcntl"
 	"github.com/goplus/llgo/c/os"
 	"unsafe"
 )
@@ -10,12 +9,11 @@ import (
 func main() {
 
 	filename := c.Str("testfile.txt")
-	data := c.Str("Hello, fcntl!")
-
+	data := c.Str("Hello, os!")
 	var buffer [20]c.Char
 
 	// Open a file, O_CREAT|O_WRONLY|O_TRUNC means create, write only, or clear the file
-	fd := fcntl.Open(filename, fcntl.O_CREAT|fcntl.O_WRONLY|fcntl.O_TRUNC, 0644)
+	fd := os.Open(filename, os.O_CREAT|os.O_WRONLY|os.O_TRUNC, 0644)
 	if fd == -1 {
 		c.Printf(c.Str("open error\n"))
 		return
@@ -31,28 +29,30 @@ func main() {
 	c.Printf(c.Str("Written %ld bytes to %s\n"), bytesWritten, filename)
 
 	// Get file status flags
-	flags := fcntl.FcNtl(fd, fcntl.F_GETFL)
+	flags := os.Fcntl(fd, os.F_GETFL)
 	if flags == -1 {
-		c.Printf(c.Str("fcntl error\n"))
+		c.Printf(c.Str("os error\n"))
 		os.Close(fd)
 		return
 	}
 	c.Printf(c.Str("File flags: %d\n"), flags)
 
 	// Set the file status flag to non-blocking mode
-	if fcntl.FcNtl(fd, fcntl.F_SETFL, flags|fcntl.O_NONBLOCK) == -1 {
-		c.Printf(c.Str("fcntl error\n"))
+	if os.Fcntl(fd, os.F_SETFL, flags|os.O_NONBLOCK) == -1 {
+		c.Printf(c.Str("os error\n"))
 		os.Close(fd)
 		return
 	}
 	c.Printf(c.Str("set file status successfully\n"))
+
+
 
 	c.Printf(c.Str("111"))
 	// Close file
 	os.Close(fd)
 
 	// Reopen the file, O_RDONLY means read-only
-	fd = fcntl.Open(filename, fcntl.O_RDONLY)
+	fd = os.Open(filename, os.O_RDONLY)
 	if fd == -1 {
 		c.Printf(c.Str("open error\n"))
 		return
