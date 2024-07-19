675d9d8c09f7be16b7d3afaf257ae9bcd65565dc
xushiwei <x@goplus.org>
2024-07-19 08:30:41 +0800

    use syscall.Timeval

--- c/sys/select.go
+++ c/sys/select.go
@@ -1,22 +1,16 @@
 package sys
 
 import (
-	"github.com/goplus/llgo/c/syscall"
 	_ "unsafe"
 
 	"github.com/goplus/llgo/c"
+	"github.com/goplus/llgo/c/syscall"
 )
 
 const (
 	LLGoFiles   = "_wrap/fddef.c"
 	LLGoPackage = "link"
 )
-
-// (TODO) merge to timeval
-type TimeVal struct {
-	TvSec  c.Long
-	TvUSec c.Int
-}
 
 //go:linkname FD_ZERO C.fd_zero
 func FD_ZERO(fdSet *syscall.FdSet)
@@ -28,4 +22,4 @@ func FD_SET(fd c.Int, fdSet *syscall.FdSet)
 func FD_ISSET(fd c.Int, fdSet *syscall.FdSet) c.Int
 
 //go:linkname Select C.select
-func Select(n c.Int, r *syscall.FdSet, w *syscall.FdSet, e *syscall.FdSet, timeout *TimeVal) c.Int
+func Select(n c.Int, r *syscall.FdSet, w *syscall.FdSet, e *syscall.FdSet, timeout *syscall.Timeval) c.Int
