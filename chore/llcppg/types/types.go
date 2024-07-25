d8026833dc509852a8c7da315050e93a3c6a6a45
xushiwei <x@goplus.org>
2024-07-25 18:46:40 +0800

    llcppg

---
+++ chore/llcppg/types/types.go
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2024 The GoPlus Authors (goplus.org). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package types
+
+// Config represents a configuration for the llcppg tool.
+type Config struct {
+	Name         string   `json:"name"`
+	CFlags       string   `json:"cflags"`
+	Libs         string   `json:"libs"`
+	Include      []string `json:"include"`
+	TrimPrefixes []string `json:"trimPrefixes"`
+}
