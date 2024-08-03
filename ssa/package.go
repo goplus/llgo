482f796bad9cdb46443600816b593ee3cbc65100
Aofei Sheng <aofei@aofeisheng.com>
2024-08-03 08:35:19 +0800

    ssa: add `llgo:link` support to `Builder.abiMthd`

--- ssa/package.go
+++ ssa/package.go
@@ -586,6 +586,7 @@ type aPackage struct {
 	named  map[types.Type]Expr
 	afterb unsafe.Pointer
 	patch  func(types.Type) types.Type
+	fnlink func(string) string
 
 	iRoutine int
 }
@@ -656,6 +657,11 @@ func (p Package) String() string {
 // SetPatch sets a patch function.
 func (p Package) SetPatch(fn func(types.Type) types.Type) {
 	p.patch = fn
+}
+
+// SetResolveLinkname sets a function to resolve linkname.
+func (p Package) SetResolveLinkname(fn func(string) string) {
+	p.fnlink = fn
 }
 
 // -----------------------------------------------------------------------------
