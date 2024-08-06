d0bf731d743a464730a80ebd9da02ad17426ab35
tsingbx <tsingbx@gmail.com>
2024-08-06 09:10:13 +0800

    change demo

--- c/openssl/_demo/chmacdemo/hmac.go
+++ c/openssl/_demo/chmacdemo/hmac.go
@@ -36,9 +36,5 @@ func main() {
 		c.Fprintf(c.Stderr, c.Str("%s\n"), c.Str("Error finalizing HMAC_CTX"))
 		return
 	}
-	fmt.Print("HMAC: ")
-	for i := 0; i < int(digestLen); i++ {
-		fmt.Printf("%02x", digest[i])
-	}
-	fmt.Print("\n")
+	fmt.Printf("HMAC:%x\n", digest[:digestLen])
 }
