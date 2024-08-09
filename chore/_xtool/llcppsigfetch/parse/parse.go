e155f5d281237c1a01ef0c5a6dbc5bd2c4b8347b
luoliwoshang <2643523683@qq.com>
2024-08-09 18:06:49 +0800

    llcppsigfetch:converter top decl

--- chore/_xtool/llcppsigfetch/parse/parse.go
+++ chore/_xtool/llcppsigfetch/parse/parse.go
@@ -3,7 +3,6 @@ package parse
 import (
 	"errors"
 
-	"github.com/goplus/llgo/c/clang"
 	"github.com/goplus/llgo/chore/llcppg/ast"
 )
 
@@ -17,41 +16,10 @@ func NewContext() *Context {
 	}
 }
 
-type Converter struct {
-	files   map[string]*ast.File
-	typeMap map[clang.Type]*ast.Expr
-	declMap map[clang.Cursor]*ast.Decl
-	curLoc  ast.Location
-}
-
-func NewConverter(filepath string) (*Converter, error) {
-	// TODO(zzy):init transUnit
-	return &Converter{
-		typeMap: make(map[clang.Type]*ast.Expr),
-		declMap: make(map[clang.Cursor]*ast.Decl),
-		files:   make(map[string]*ast.File),
-	}, nil
-}
-
-func (c *Converter) Dispose() {
-	// TODO(zzy): clean like transUnit
-}
-func (c *Converter) Convert() (map[string]*ast.File, error) {
-	// TODO(zzy): main logic to convert
-
-	// c.UpdateLocation()
-
-	return nil, nil
-}
-
-func (c *Converter) UpdateLocation(loc ast.Location) {
-	c.curLoc = loc
-}
-
 // ProcessFiles processes the given files and adds them to the context
-func (c *Context) ProcessFiles(files []string) error {
+func (p *Context) ProcessFiles(files []string) error {
 	for _, file := range files {
-		if err := c.processFile(file); err != nil {
+		if err := p.processFile(file); err != nil {
 			return err
 		}
 	}
@@ -59,24 +27,24 @@ func (c *Context) ProcessFiles(files []string) error {
 }
 
 // parse file and add it to the context,avoid duplicate parsing
-func (c *Context) processFile(path string) error {
-	if _, exists := c.Files[path]; exists {
+func (p *Context) processFile(path string) error {
+	if _, exists := p.Files[path]; exists {
 		return nil
 	}
-	parsedfiles, err := parseFile(path)
+	parsedfiles, err := p.parseFile(path)
 	if err != nil {
 		return errors.New("failed to parse file: " + path)
 	}
 
 	for path, file := range parsedfiles {
-		if _, exist := c.Files[path]; !exist {
-			c.Files[path] = file
+		if _, exist := p.Files[path]; !exist {
+			p.Files[path] = file
 		}
 	}
 	return nil
 }
 
-func parseFile(path string) (map[string]*ast.File, error) {
+func (p *Context) parseFile(path string) (map[string]*ast.File, error) {
 	converter, err := NewConverter(path)
 	if err != nil {
 		return nil, errors.New("failed to create converter " + path)
