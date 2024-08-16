2b1d4b667234b15bdb0868143ee53436f2b3ee1e
luoliwoshang <2643523683@qq.com>
2024-08-16 11:52:22 +0800

    llcppsigfetch:output result

--- chore/_xtool/llcppsigfetch/parse/dump.go
+++ chore/_xtool/llcppsigfetch/parse/dump.go
@@ -6,66 +6,73 @@ import (
 	"github.com/goplus/llgo/chore/llcppg/ast"
 )
 
-func (ct *Converter) GetFilesJSON() *cjson.JSON {
+func MarshalASTFiles(files map[string]*ast.File) *cjson.JSON {
 	root := cjson.Object()
-	for _, file := range ct.files {
-		root.SetItem(c.AllocaCStr(file.Path), ct.FileJSON(file))
+	for _, file := range files {
+		root.SetItem(c.AllocaCStr(file.Path), MarshalASTFile(file))
 	}
 	return root
 }
 
-func (ct *Converter) FileJSON(file *ast.File) *cjson.JSON {
+func MarshalASTFile(file *ast.File) *cjson.JSON {
 	root := cjson.Object()
 	decls := cjson.Array()
-	includes := cjson.Array()
-	macros := cjson.Array()
 
 	for _, decl := range file.Decls {
-		decls.AddItem(ct.DeclJSON(decl))
+		decls.AddItem(MarshalASTDecl(decl))
 	}
 
 	root.SetItem(c.Str("path"), cjson.String(c.AllocaCStr(file.Path)))
 	root.SetItem(c.Str("decls"), decls)
-	root.SetItem(c.Str("includes"), includes)
-	root.SetItem(c.Str("macros"), macros)
+
+	// json:includes,omitempty
+	if file.Includes != nil {
+		includes := cjson.Array()
+		root.SetItem(c.Str("includes"), includes)
+	}
+
+	// json:macros,omitempty
+	if file.Macros != nil {
+		macros := cjson.Array()
+		root.SetItem(c.Str("macros"), macros)
+	}
+
 	return root
 }
 
-func (ct *Converter) DeclJSON(decl ast.Decl) *cjson.JSON {
+func MarshalASTDecl(decl ast.Decl) *cjson.JSON {
 	if decl == nil {
 		return cjson.Null()
 	}
 	root := cjson.Object()
 	switch d := decl.(type) {
 	case *ast.FuncDecl:
-		ct.DeclBaseJSON(d.DeclBase, root)
-		root.SetItem(c.Str("Name"), ct.TypeJSON(d.Name))
-		root.SetItem(c.Str("Type"), ct.TypeJSON(d.Type))
+		MarshalASTDeclBase(d.DeclBase, root)
+		root.SetItem(c.Str("Name"), MarshalASTExpr(d.Name))
+		root.SetItem(c.Str("Type"), MarshalASTExpr(d.Type))
 	case *ast.TypeDecl:
-		ct.DeclBaseJSON(d.DeclBase, root)
+		MarshalASTDeclBase(d.DeclBase, root)
 		root.SetItem(c.Str("Tag"), cjson.Number(float64(d.Tag)))
-		root.SetItem(c.Str("Fields"), ct.TypeJSON(d.Fields))
+		root.SetItem(c.Str("Fields"), MarshalASTExpr(d.Fields))
 		methods := cjson.Array()
 		for _, m := range d.Methods {
-			methods.AddItem(ct.DeclJSON(m))
+			methods.AddItem(MarshalASTDecl(m))
 		}
 		root.SetItem(c.Str("Methods"), methods)
 	}
 	return root
 }
-func (ct *Converter) DeclBaseJSON(decl ast.DeclBase, root *cjson.JSON) {
-	if decl.Loc == nil {
-		root.SetItem(c.Str("Loc"), cjson.Null())
-	} else {
-		loc := cjson.Object()
-		loc.SetItem(c.Str("File"), cjson.String(c.AllocaCStr(decl.Loc.File)))
-		root.SetItem(c.Str("Loc"), loc)
-	}
-	root.SetItem(c.Str("Doc"), ct.TypeJSON(decl.Doc))
-	root.SetItem(c.Str("Parent"), ct.TypeJSON(decl.Parent))
+
+func MarshalASTDeclBase(decl ast.DeclBase, root *cjson.JSON) {
+	loc := cjson.Object()
+	loc.SetItem(c.Str("File"), cjson.String(c.AllocaCStr(decl.Loc.File)))
+	root.SetItem(c.Str("Loc"), loc)
+
+	root.SetItem(c.Str("Doc"), MarshalASTExpr(decl.Doc))
+	root.SetItem(c.Str("Parent"), MarshalASTExpr(decl.Parent))
 }
 
-func (ct *Converter) TypeJSON(t ast.Expr) *cjson.JSON {
+func MarshalASTExpr(t ast.Expr) *cjson.JSON {
 	if t == nil {
 		return cjson.Null()
 	}
@@ -74,24 +81,24 @@ func (ct *Converter) TypeJSON(t ast.Expr) *cjson.JSON {
 
 	switch d := t.(type) {
 	case *ast.FuncType:
-		root.SetItem(c.Str("Params"), ct.TypeJSON(d.Params))
-		root.SetItem(c.Str("Ret"), ct.TypeJSON(d.Ret))
+		root.SetItem(c.Str("Params"), MarshalASTExpr(d.Params))
+		root.SetItem(c.Str("Ret"), MarshalASTExpr(d.Ret))
 	case *ast.FieldList:
 		if d == nil {
 			return cjson.Null()
 		}
 		list := cjson.Array()
 		for _, f := range d.List {
-			list.AddItem(ct.TypeJSON(f))
+			list.AddItem(MarshalASTExpr(f))
 		}
 		root.SetItem(c.Str("List"), list)
 	case *ast.Field:
-		root.SetItem(c.Str("Type"), ct.TypeJSON(d.Type))
-		root.SetItem(c.Str("Doc"), ct.TypeJSON(d.Doc))
-		root.SetItem(c.Str("Comment"), ct.TypeJSON(d.Comment))
+		root.SetItem(c.Str("Type"), MarshalASTExpr(d.Type))
+		root.SetItem(c.Str("Doc"), MarshalASTExpr(d.Doc))
+		root.SetItem(c.Str("Comment"), MarshalASTExpr(d.Comment))
 		names := cjson.Array()
 		for _, n := range d.Names {
-			names.AddItem(ct.TypeJSON(n))
+			names.AddItem(MarshalASTExpr(n))
 		}
 		root.SetItem(c.Str("Names"), names)
 	case *ast.Ident:
@@ -100,10 +107,10 @@ func (ct *Converter) TypeJSON(t ast.Expr) *cjson.JSON {
 		root.SetItem(c.Str("Kind"), cjson.Number(float64(d.Kind)))
 		root.SetItem(c.Str("Value"), cjson.String(c.AllocaCStr(d.Value)))
 	case *ast.PointerType:
-		root.SetItem(c.Str("X"), ct.TypeJSON(d.X))
+		root.SetItem(c.Str("X"), MarshalASTExpr(d.X))
 	case *ast.ArrayType:
-		root.SetItem(c.Str("Elt"), ct.TypeJSON(d.Elt))
-		root.SetItem(c.Str("Len"), ct.TypeJSON(d.Len))
+		root.SetItem(c.Str("Elt"), MarshalASTExpr(d.Elt))
+		root.SetItem(c.Str("Len"), MarshalASTExpr(d.Len))
 	case *ast.BuiltinType:
 		root.SetItem(c.Str("Kind"), cjson.Number(float64(d.Kind)))
 		root.SetItem(c.Str("Flags"), cjson.Number(float64(d.Flags)))
@@ -115,12 +122,12 @@ func (ct *Converter) TypeJSON(t ast.Expr) *cjson.JSON {
 		}
 		list := cjson.Array()
 		for _, c := range d.List {
-			list.AddItem(ct.TypeJSON(c))
+			list.AddItem(MarshalASTExpr(c))
 		}
 		root.SetItem(c.Str("List"), list)
 	case *ast.ScopingExpr:
-		root.SetItem(c.Str("X"), ct.TypeJSON(d.X))
-		root.SetItem(c.Str("Parent"), ct.TypeJSON(d.Parent))
+		root.SetItem(c.Str("X"), MarshalASTExpr(d.X))
+		root.SetItem(c.Str("Parent"), MarshalASTExpr(d.Parent))
 	default:
 		return cjson.Null()
 	}
