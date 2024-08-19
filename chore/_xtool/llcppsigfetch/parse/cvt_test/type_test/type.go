f0e92343cb9aaac0a2b99b7ecdebbfee211d54d6
luoliwoshang <2643523683@qq.com>
2024-08-19 21:39:42 +0800

    llcppsigfetch:complex

--- chore/_xtool/llcppsigfetch/parse/cvt_test/type_test/type.go
+++ chore/_xtool/llcppsigfetch/parse/cvt_test/type_test/type.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"unsafe"
 
 	"github.com/goplus/llgo/c/clang"
 	"github.com/goplus/llgo/chore/_xtool/llcppsigfetch/parse"
@@ -14,7 +15,7 @@ func main() {
 func TestBuiltinType() {
 	tests := []struct {
 		name     string
-		typ      *clang.Type
+		typ      clang.Type
 		expected ast.BuiltinType
 	}{
 		{"Void", btType(clang.TypeVoid), ast.BuiltinType{Kind: ast.Void}},
@@ -40,13 +41,16 @@ func TestBuiltinType() {
 		{"Double", btType(clang.TypeDouble), ast.BuiltinType{Kind: ast.Float, Flags: ast.Double}},
 		{"LongDouble", btType(clang.TypeLongDouble), ast.BuiltinType{Kind: ast.Float, Flags: ast.Long | ast.Double}},
 		{"Float128", btType(clang.TypeFloat128), ast.BuiltinType{Kind: ast.Float128}},
+		{"Complex", mockComplexType(0), ast.BuiltinType{Kind: ast.Complex}},
+		{"Complex", mockComplexType(ast.Double), ast.BuiltinType{Flags: ast.Double, Kind: ast.Complex}},
+		{"Complex", mockComplexType(ast.Long | ast.Double), ast.BuiltinType{Flags: ast.Long | ast.Double, Kind: ast.Complex}},
 		{"Unknown", btType(clang.TypeIbm128), ast.BuiltinType{Kind: ast.Void}},
 	}
 
 	converter := &parse.Converter{}
 	converter.Convert()
 	for _, bt := range tests {
-		res := converter.ProcessBuiltinType(*bt.typ)
+		res := converter.ProcessBuiltinType(bt.typ)
 		if res.Kind != bt.expected.Kind {
 			fmt.Printf("%s Kind mismatch:got %d want %d, \n", bt.name, res.Kind, bt.expected.Kind)
 		}
@@ -57,6 +61,44 @@ func TestBuiltinType() {
 	}
 }
 
-func btType(kind clang.TypeKind) *clang.Type {
-	return &clang.Type{Kind: kind}
+func btType(kind clang.TypeKind) clang.Type {
+	return clang.Type{Kind: kind}
+}
+
+func visit(cursor, parent clang.Cursor, clientData unsafe.Pointer) clang.ChildVisitResult {
+	typ := (*clang.Type)(clientData)
+	if cursor.Kind == clang.CursorVarDecl && cursor.Type().Kind == clang.TypeComplex {
+		*typ = cursor.Type()
+	}
+	return clang.ChildVisit_Continue
+}
+
+// mock complex type, this type cannot be directly created in Go
+func mockComplexType(flag ast.TypeFlag) clang.Type {
+	var typeStr string
+	if flag&(ast.Long|ast.Double) == (ast.Long | ast.Double) {
+		typeStr = "long double"
+	} else if flag&ast.Double != 0 {
+		typeStr = "double"
+	} else {
+		typeStr = "float"
+	}
+
+	code := fmt.Sprintf("#include <complex.h>\n%s complex z;", typeStr)
+	index, unit, err := parse.CreateTranslationUnit(&parse.Config{
+		File: code,
+		Temp: true,
+		Args: []string{"-x", "c", "-std=c99"},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	defer index.Dispose()
+
+	cursor := unit.Cursor()
+	complex := &clang.Type{}
+	clang.VisitChildren(cursor, visit, unsafe.Pointer(complex))
+
+	return *complex
 }
