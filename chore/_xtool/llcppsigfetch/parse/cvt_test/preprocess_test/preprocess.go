43c74a4e2edd99e1e11cdcdedd4d19040cc9ed80
luoliwoshang <2643523683@qq.com>
2024-08-20 10:08:25 +0800

    llcppsigfetch:marco tokens & refine test

--- chore/_xtool/llcppsigfetch/parse/cvt_test/preprocess_test/preprocess.go
+++ chore/_xtool/llcppsigfetch/parse/cvt_test/preprocess_test/preprocess.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"github.com/goplus/llgo/c"
-	"github.com/goplus/llgo/chore/_xtool/llcppsigfetch/parse"
+	test "github.com/goplus/llgo/chore/_xtool/llcppsigfetch/parse/cvt_test"
 )
 
 func main() {
@@ -12,29 +11,11 @@ func main() {
 
 func TestDefine() {
 	testCases := []string{
+		`#define DEBUG`,
 		`#define OK 1`,
 		`#define SQUARE(x) ((x) * (x))`,
 	}
-
-	for i, content := range testCases {
-		converter, err := parse.NewConverter(&parse.Config{
-			File: content,
-			Temp: true,
-		})
-		if err != nil {
-			panic(err)
-		}
-
-		_, err = converter.Convert()
-		if err != nil {
-			panic(err)
-		}
-
-		json := converter.MarshalASTFiles()
-		c.Printf(c.Str("TestDefine Case %d:\n%s\n\n"), c.Int(i+1), json.Print())
-
-		converter.Dispose()
-	}
+	test.RunTest("TestDefine", testCases)
 }
 
 func TestInclude() {
@@ -42,24 +23,5 @@ func TestInclude() {
 		`#include "foo.h"`,
 		// `#include <limits.h>`, //  Standard libraries are mostly platform-dependent
 	}
-
-	for i, content := range testCases {
-		converter, err := parse.NewConverter(&parse.Config{
-			File: content,
-			Temp: true,
-		})
-		if err != nil {
-			panic(err)
-		}
-
-		_, err = converter.Convert()
-		if err != nil {
-			panic(err)
-		}
-
-		json := converter.MarshalASTFiles()
-		c.Printf(c.Str("TestInclude Case %d:\n%s\n\n"), c.Int(i+1), json.Print())
-
-		converter.Dispose()
-	}
+	test.RunTest("TestInclude", testCases)
 }
