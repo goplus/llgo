43aae490557766bad4422d22a8102e97af506bee
luoliwoshang <2643523683@qq.com>
2024-08-26 14:17:33 +0800

    llcppsigfetch:collect public methods

--- chore/_xtool/llcppsigfetch/parse/cvt_test/decl_test/class_test/class.go
+++ chore/_xtool/llcppsigfetch/parse/cvt_test/decl_test/class_test/class.go
@@ -9,26 +9,33 @@ func main() {
 func TestClassDecl() {
 	testCases := []string{
 		`class A {
+		public:
 			int a;
 			int b;
 		};`,
 		`class A {
+		public:
 			int a;
 			int b;
 			float foo(int a,double b);
+		private:
+			void bar();
 		};`,
 		`class A {
+		public:
 			A();
 			explicit A();
 			~A();
 			static inline void foo();
 		};`,
 		`class Base {
+		public:
 			Base();
 			virtual ~Base();
 			virtual void foo();
 		};
 		class Derived : public Base {
+		public:
 			Derived();
 			~Derived() override;
 			void foo() override;
