ed8eb4b17339894cb9a07ff3fe53a5cf04b813e2
luoliwoshang <2643523683@qq.com>
2024-08-28 18:50:05 +0800

    llcppsigfetch:dump node type for unmarshal

--- chore/_xtool/llcppsigfetch/parse/dump.go
+++ chore/_xtool/llcppsigfetch/parse/dump.go
@@ -30,6 +30,7 @@ func MarshalASTFile(file *ast.File) *cjson.JSON {
 		includes := cjson.Array()
 		for _, i := range file.Includes {
 			include := cjson.Object()
+			include.SetItem(c.Str("_Type"), stringField("Include"))
 			include.SetItem(c.Str("Path"), stringField(i.Path))
 			includes.AddItem(include)
 		}
@@ -41,6 +42,7 @@ func MarshalASTFile(file *ast.File) *cjson.JSON {
 		macros := cjson.Array()
 		for _, m := range file.Macros {
 			marco := cjson.Object()
+			marco.SetItem(c.Str("_Type"), stringField("Macro"))
 			marco.SetItem(c.Str("Name"), stringField(m.Name))
 			tokens := cjson.Array()
 			for _, tok := range m.Tokens {
@@ -55,6 +57,7 @@ func MarshalASTFile(file *ast.File) *cjson.JSON {
 }
 func Token(tok *ast.Token) *cjson.JSON {
 	root := cjson.Object()
+	root.SetItem(c.Str("_Type"), stringField("Token"))
 	root.SetItem(c.Str("Token"), numberField(uint(tok.Token)))
 	root.SetItem(c.Str("Lit"), stringField(tok.Lit))
 	return root
@@ -67,14 +70,17 @@ func MarshalASTDecl(decl ast.Decl) *cjson.JSON {
 	root := cjson.Object()
 	switch d := decl.(type) {
 	case *ast.EnumTypeDecl:
+		root.SetItem(c.Str("_Type"), stringField("EnumTypeDecl"))
 		MarshalASTDeclBase(d.DeclBase, root)
 		root.SetItem(c.Str("Name"), MarshalASTExpr(d.Name))
 		root.SetItem(c.Str("Type"), MarshalASTExpr(d.Type))
 	case *ast.TypedefDecl:
+		root.SetItem(c.Str("_Type"), stringField("TypedefDecl"))
 		MarshalASTDeclBase(d.DeclBase, root)
 		root.SetItem(c.Str("Name"), MarshalASTExpr(d.Name))
 		root.SetItem(c.Str("Type"), MarshalASTExpr(d.Type))
 	case *ast.FuncDecl:
+		root.SetItem(c.Str("_Type"), stringField("FuncDecl"))
 		MarshalASTDeclBase(d.DeclBase, root)
 		root.SetItem(c.Str("Name"), MarshalASTExpr(d.Name))
 		root.SetItem(c.Str("Type"), MarshalASTExpr(d.Type))
@@ -87,6 +93,7 @@ func MarshalASTDecl(decl ast.Decl) *cjson.JSON {
 		root.SetItem(c.Str("IsVirtual"), boolField(d.IsVirtual))
 		root.SetItem(c.Str("IsOverride"), boolField(d.IsOverride))
 	case *ast.TypeDecl:
+		root.SetItem(c.Str("_Type"), stringField("TypeDecl"))
 		MarshalASTDeclBase(d.DeclBase, root)
 		root.SetItem(c.Str("Name"), MarshalASTExpr(d.Name))
 		root.SetItem(c.Str("Type"), MarshalASTExpr(d.Type))
@@ -96,6 +103,7 @@ func MarshalASTDecl(decl ast.Decl) *cjson.JSON {
 
 func MarshalASTDeclBase(decl ast.DeclBase, root *cjson.JSON) {
 	loc := cjson.Object()
+	loc.SetItem(c.Str("_Type"), stringField("Location"))
 	loc.SetItem(c.Str("File"), stringField(decl.Loc.File))
 	root.SetItem(c.Str("Loc"), loc)
 
@@ -112,15 +120,18 @@ func (ct *Converter) TypeJSON(t ast.Expr) *cjson.JSON {
 
 	switch d := t.(type) {
 	case *ast.EnumType:
+		root.SetItem(c.Str("_Type"), stringField("EnumType"))
 		items := cjson.Array()
 		for _, e := range d.Items {
 			items.AddItem(MarshalASTExpr(e))
 		}
 		root.SetItem(c.Str("Items"), items)
 	case *ast.EnumItem:
+		root.SetItem(c.Str("_Type"), stringField("EnumItem"))
 		root.SetItem(c.Str("Name"), MarshalASTExpr(d.Name))
 		root.SetItem(c.Str("Value"), MarshalASTExpr(d.Value))
 	case *ast.RecordType:
+		root.SetItem(c.Str("_Type"), stringField("RecordType"))
 		root.SetItem(c.Str("Tag"), numberField(uint(d.Tag)))
 		root.SetItem(c.Str("Fields"), MarshalASTExpr(d.Fields))
 		methods := cjson.Array()
@@ -129,9 +140,11 @@ func (ct *Converter) TypeJSON(t ast.Expr) *cjson.JSON {
 		}
 		root.SetItem(c.Str("Methods"), methods)
 	case *ast.FuncType:
+		root.SetItem(c.Str("_Type"), stringField("FuncType"))
 		root.SetItem(c.Str("Params"), MarshalASTExpr(d.Params))
 		root.SetItem(c.Str("Ret"), MarshalASTExpr(d.Ret))
 	case *ast.FieldList:
+		root.SetItem(c.Str("_Type"), stringField("FieldList"))
 		if d == nil {
 			return cjson.Null()
 		}
@@ -141,6 +154,7 @@ func (ct *Converter) TypeJSON(t ast.Expr) *cjson.JSON {
 		}
 		root.SetItem(c.Str("List"), list)
 	case *ast.Field:
+		root.SetItem(c.Str("_Type"), stringField("Field"))
 		root.SetItem(c.Str("Type"), MarshalASTExpr(d.Type))
 		root.SetItem(c.Str("Doc"), MarshalASTExpr(d.Doc))
 		root.SetItem(c.Str("Comment"), MarshalASTExpr(d.Comment))
@@ -152,24 +166,32 @@ func (ct *Converter) TypeJSON(t ast.Expr) *cjson.JSON {
 		}
 		root.SetItem(c.Str("Names"), names)
 	case *ast.Variadic:
+		root.SetItem(c.Str("_Type"), stringField("Variadic"))
 	case *ast.Ident:
+		root.SetItem(c.Str("_Type"), stringField("Ident"))
 		if d == nil {
 			return cjson.Null()
 		}
 		root.SetItem(c.Str("Name"), stringField(d.Name))
 	case *ast.TagExpr:
+		root.SetItem(c.Str("_Type"), stringField("TagExpr"))
 		root.SetItem(c.Str("Name"), MarshalASTExpr(d.Name))
 		root.SetItem(c.Str("Tag"), numberField(uint(d.Tag)))
 	case *ast.BasicLit:
+		root.SetItem(c.Str("_Type"), stringField("BasicLit"))
 		root.SetItem(c.Str("Kind"), numberField(uint(d.Kind)))
 		root.SetItem(c.Str("Value"), stringField(d.Value))
 	case *ast.LvalueRefType:
+		root.SetItem(c.Str("_Type"), stringField("LvalueRefType"))
 		root.SetItem(c.Str("X"), MarshalASTExpr(d.X))
 	case *ast.RvalueRefType:
+		root.SetItem(c.Str("_Type"), stringField("RvalueRefType"))
 		root.SetItem(c.Str("X"), MarshalASTExpr(d.X))
 	case *ast.PointerType:
+		root.SetItem(c.Str("_Type"), stringField("PointerType"))
 		root.SetItem(c.Str("X"), MarshalASTExpr(d.X))
 	case *ast.ArrayType:
+		root.SetItem(c.Str("_Type"), stringField("ArrayType"))
 		root.SetItem(c.Str("Elt"), MarshalASTExpr(d.Elt))
 		root.SetItem(c.Str("Len"), MarshalASTExpr(d.Len))
 	case *ast.BuiltinType:
@@ -177,6 +199,7 @@ func (ct *Converter) TypeJSON(t ast.Expr) *cjson.JSON {
 		root.SetItem(c.Str("Kind"), numberField(uint(d.Kind)))
 		root.SetItem(c.Str("Flags"), numberField(uint(d.Flags)))
 	case *ast.Comment:
+		root.SetItem(c.Str("_Type"), stringField("Comment"))
 		if d == nil {
 			return cjson.Null()
 		}
@@ -193,6 +216,7 @@ func (ct *Converter) TypeJSON(t ast.Expr) *cjson.JSON {
 		}
 		root.SetItem(c.Str("List"), list)
 	case *ast.ScopingExpr:
+		root.SetItem(c.Str("_Type"), stringField("ScopingExpr"))
 		root.SetItem(c.Str("X"), MarshalASTExpr(d.X))
 		root.SetItem(c.Str("Parent"), MarshalASTExpr(d.Parent))
 	default:
