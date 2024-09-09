2e04df4d5d1a66ab33af275849d17f09716e0a88
luoliwoshang <2643523683@qq.com>
2024-09-09 16:34:26 +0800

    llcppsigfetch:avoid tab

--- chore/_xtool/llcppsigfetch/parse/cvt_test/decl_test/comment_test/comment.go
+++ chore/_xtool/llcppsigfetch/parse/cvt_test/decl_test/comment_test/comment.go
@@ -8,26 +8,35 @@ func main() {
 
 func TestDoc() {
 	testCases := []string{
-		`// not read doc 1
-		 void foo();`,
-		`/* not read doc 2 */
-		 void foo();`,
-		`/// doc
-		 void foo();`,
-		`/** doc */
-		 void foo();`,
-		`/*! doc */
-		 void foo();`,
-		`/// doc 1
+		`
+// not read doc 1
+void foo();`,
+		`
+/* not read doc 2 */
+void foo();`,
+		`
+/// doc
+void foo();`,
+		`
+/** doc */
+void foo();`,
+		`
+/*! doc */
+void foo();`,
+		`
+/// doc 1
 /// doc 2
 void foo();`,
-		`/*! doc 1 */
+		`
+/*! doc 1 */
 /*! doc 2 */
 void foo();`,
-		`/** doc 1 */
+		`
+/** doc 1 */
 /** doc 1 */
 void foo();`,
-		`/**
+		`
+/**
  * doc 1
  * doc 2
  */
