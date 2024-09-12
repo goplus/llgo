04db311a2fe34fb0bc259f88d0356b5538746591
tsingbx <tsingbx@gmail.com>
2024-09-12 15:26:35 +0800

    add go:linkname for func decl

---
+++ chore/gogensig/visitor/genpkg/comments.go
@@ -0,0 +1,29 @@
+package genpkg
+
+import (
+	goast "go/ast"
+	"strings"
+)
+
+type FuncNameType string
+
+func toTitle(s string) string {
+	return strings.ToUpper(s[:1]) + strings.ToLower(s[1:])
+}
+
+func toGoFuncName(funcName string) string {
+	subs := strings.Split(string(funcName), "_")
+	name := ""
+	for _, sub := range subs {
+		name += toTitle(sub)
+	}
+	return name
+}
+
+func NewFuncDocComments(funcName string) *goast.CommentGroup {
+	goFuncName := toGoFuncName(funcName)
+	txt := "\n//go:linkname " + goFuncName + " " + "C." + funcName
+	comment := goast.Comment{Text: txt}
+	commentGroup := goast.CommentGroup{List: []*goast.Comment{&comment}}
+	return &commentGroup
+}
