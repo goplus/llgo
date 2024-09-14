758f5538f5cc57fce9cfd299d338d530c7a04faf
tsingbx <tsingbx@gmail.com>
2024-09-14 18:46:21 +0800

    refactoring
    rename _testjson to _testinput dir name
    
    add ToInt for Expr
    
    add test for convert types
    
    rename subtest case name
    
    add ToChar subtest
    
    Refactoring
    
    add ToArrayType
    
    rename convert type
    
    rename
    
    add TypeConv
    
    move symbolTable
    
    remove typeBlock
    
    move comments.go
    
    add MangledName handle for FuncDecl
    
    add Delete
    
    change LookupSymbol
    
    rename ToGoFuncName
    
    remove expr ToBuiltinType

--- chore/gogensig/visitor/testvisitor/enum_test.go
+++ chore/gogensig/visitor/testvisitor/enum_test.go
@@ -1,16 +1,21 @@
-package genpkg_test
+package testvisitor_test
 
 import (
 	"testing"
 
 	"github.com/goplus/llgo/chore/gogensig/unmarshal"
+	"github.com/goplus/llgo/chore/gogensig/util"
 	"github.com/goplus/llgo/chore/gogensig/visitor"
 )
 
 func TestSpectrum(t *testing.T) {
-	docPath := "./_enumtest/spectrum.json"
+	docPath := "./_testinput/_enumtest/spectrum.h"
 	astConvert := visitor.NewAstConvert("spectrum", docPath)
 	docVisitors := []visitor.DocVisitor{astConvert}
 	p := unmarshal.NewDocFileSetUnmarshaller(docVisitors)
-	p.UnmarshalFile(docPath)
+	bytes, err := util.Llcppsigfetch(docPath, false, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	p.UnmarshalBytes(bytes)
 }
