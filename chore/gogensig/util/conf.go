7fe5c3d4211b9db1bd8caa04c08cebca5b687bc0
luoliwoshang <2643523683@qq.com>
2024-09-20 08:46:01 +0800

    gogensig:config util & link full operation
    gogensig:unsafe sizeof 0
    
    gogensig:temp link lib const
    
    llcppg:pipe writer close

---
+++ chore/gogensig/util/conf.go
@@ -0,0 +1,20 @@
+package util
+
+import (
+	"encoding/json"
+
+	cppgtypes "github.com/goplus/llgo/chore/llcppg/types"
+)
+
+func GetCppgFromPath(filePath string) (*cppgtypes.Config, error) {
+	bytes, err := ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	conf := &cppgtypes.Config{}
+	err = json.Unmarshal(bytes, &conf)
+	if err != nil {
+		return nil, err
+	}
+	return conf, nil
+}
