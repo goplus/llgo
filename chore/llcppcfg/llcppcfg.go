1f76e832372d8b528e0eff6dff57a6d1e0bb13e1
tsingbx <tsingbx@gmail.com>
2024-09-30 19:14:54 +0800

    add llcppcfg command to generate llcpp.cfg file

---
+++ chore/llcppcfg/llcppcfg.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+type LLCppConfig struct {
+	Name         string   `json:"name"`
+	Cflags       string   `json:"cflags"`
+	Include      []string `json:"include"`
+	Libs         string   `json:"libs"`
+	TrimPrefixes []string `json:"trim_prefixes"`
+	Cplusplus    bool     `json:"cplusplus"`
+}
+
+func NewLLCppConfig(name string, isCpp bool) *LLCppConfig {
+	cfg := &LLCppConfig{
+		Name: name,
+	}
+	cfg.Cflags = fmt.Sprintf("$(pkg-config --cflags %s)", name)
+	cfg.Libs = fmt.Sprintf("$(pkg-config --libs %s)", name)
+	cfg.TrimPrefixes = []string{}
+	cfg.Cplusplus = isCpp
+	cfg.Include = []string{}
+	return cfg
+}
+
+func GenCfg(name string) (*bytes.Buffer, error) {
+	if len(name) <= 0 {
+		return nil, fmt.Errorf("name can't be empty")
+	}
+	cfg := NewLLCppConfig(name, true)
+	buf := bytes.NewBuffer([]byte{})
+	jsonEncoder := json.NewEncoder(buf)
+	jsonEncoder.SetIndent("", "\t")
+	err := jsonEncoder.Encode(cfg)
+	if err != nil {
+		return nil, err
+	}
+	return buf, nil
+}
+
+func main() {
+	flag.Parse()
+	name := ""
+	if len(flag.Args()) > 0 {
+		name = flag.Arg(0)
+	}
+	buf, err := GenCfg(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	outFile := "./llcppg.cfg"
+	err = os.WriteFile(outFile, buf.Bytes(), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Config file has been generated at ", outFile, "!")
+}
