e6fb69213e87da8b73ec31fa91096496bf645663
tsingbx <tsingbx@gmail.com>
2024-10-08 19:19:22 +0800

    improve llcppcfg to print help message and handle -cpp flag

--- chore/llcppcfg/llcppcfg.go
+++ chore/llcppcfg/llcppcfg.go
@@ -2,19 +2,37 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/goplus/llgo/chore/llcppcfg/cfg"
 )
 
+func printHelp() {
+	flag.Usage()
+	flag.PrintDefaults()
+}
+
 func main() {
+	var cpp bool = false
+	var help bool = false
+	flag.BoolVar(&cpp, "cpp", false, "if it is c++ lib")
+	flag.BoolVar(&help, "help", false, "print help message")
+	flag.Usage = func() {
+		fmt.Println(`llcppcfg is to generate llcpp.cfg file.
+usage: llcppcfg [-cpp|-help] libname`)
+	}
 	flag.Parse()
+	if help || len(os.Args) <= 1 {
+		printHelp()
+		return
+	}
 	name := ""
 	if len(flag.Args()) > 0 {
 		name = flag.Arg(0)
 	}
-	buf, err := cfg.GenCfg(name)
+	buf, err := cfg.GenCfg(name, cpp)
 	if err != nil {
 		log.Fatal(err)
 	}
