051e6d2decdfeb22595904d38b329654d0f931b4
Li Jie <cpunion@gmail.com>
2024-10-24 23:18:53 +0800

    cgo: replace go std lib

--- build.sh
+++ build.sh
@@ -2,24 +2,29 @@
 
 DIR="$PWD"
 
-LLGO_PATH="$(dirname "$(readlink -f "$0")")"
+LLGOROOT="$(dirname "$(readlink -f "$0")")"
 GOROOT="$(go env GOROOT)"
-TEST_GO_PATH="$LLGO_PATH/.golang"
-mkdir -p "$TEST_GO_PATH"
-rm -rf "${TEST_GO_PATH:?}"/*
+WRAPPED_GOROOT="$LLGOROOT/.golang"
+mkdir -p "$WRAPPED_GOROOT"
+rm -rf "${WRAPPED_GOROOT:?}"/*
 
 # Create symlinks for all directories and files in GOROOT, except for the pkg directory
 for item in "$GOROOT"/*; do
-    if [ "$(basename "$item")" != "pkg" ]; then
-        ln -s "$item" "$TEST_GO_PATH/$(basename "$item")"
+    if [ "$(basename "$item")" != "pkg" ] && [ "$(basename "$item")" != "src" ]; then
+        ln -s "$item" "$WRAPPED_GOROOT/$(basename "$item")"
     fi
 done
 
+mkdir -p "$WRAPPED_GOROOT/src"
+ln -s "$LLGOROOT/internal/lib"/* "$WRAPPED_GOROOT/src/"
+ln -s "$GOROOT/src/errors" "$WRAPPED_GOROOT/src/"
+ln -s "$GOROOT/src/unsafe" "$WRAPPED_GOROOT/src/"
+
 # Special handling for the pkg/tool/darwin_arm64 directory
-mkdir -p "$TEST_GO_PATH/pkg/tool/darwin_arm64"
+mkdir -p "$WRAPPED_GOROOT/pkg/tool/darwin_arm64"
 for item in "$GOROOT"/pkg/*; do
     if [ "$(basename "$item")" != "tool" ]; then
-        ln -s "$item" "$TEST_GO_PATH/pkg/$(basename "$item")"
+        ln -s "$item" "$WRAPPED_GOROOT/pkg/$(basename "$item")"
     fi
 done
 
@@ -27,23 +32,23 @@ done
 for item in "$GOROOT"/pkg/tool/darwin_arm64/*; do
     filename=$(basename "$item")
     if [ "$filename" != "cgo" ] && [ "$filename" != "compile" ] && [ "$filename" != "link" ]; then
-        ln -s "$item" "$TEST_GO_PATH/pkg/tool/darwin_arm64/$filename"
+        ln -s "$item" "$WRAPPED_GOROOT/pkg/tool/darwin_arm64/$filename"
     fi
 done
 
 set -x
 set -e
 
-cd "$LLGO_PATH" || exit
-go build -o "$TEST_GO_PATH/pkg/tool/darwin_arm64/cgo" ./cmd/llcgo
-go build -o "$TEST_GO_PATH/pkg/tool/darwin_arm64/compile" ./cmd/llgoc
-go build -o "$TEST_GO_PATH/pkg/tool/darwin_arm64/link" ./cmd/llgolink
+cd "$LLGOROOT" || exit
+go build -o "$WRAPPED_GOROOT/pkg/tool/darwin_arm64/cgo" ./cmd/llcgo
+go build -o "$WRAPPED_GOROOT/pkg/tool/darwin_arm64/compile" ./cmd/llgoc
+go build -o "$WRAPPED_GOROOT/pkg/tool/darwin_arm64/link" ./cmd/llgolink
 
 cd "$DIR" || exit
-export GOROOT="$TEST_GO_PATH"
+export GOROOT="$WRAPPED_GOROOT"
 
-"$TEST_GO_PATH/bin/go" env
+"$WRAPPED_GOROOT/bin/go" env
 
 echo "go build $* ..."
 
-"$TEST_GO_PATH/bin/go" build -x -v -work -p 1 "$@"
+"$WRAPPED_GOROOT/bin/go" build -x -v -work -p 1 "$@"
