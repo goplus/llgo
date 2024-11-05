82e771bab77b4648186fa850f163967af46f5028
luoliwoshang <2643523683@qq.com>
2024-11-05 14:43:02 +0800

    chore:temp workflow for llcppsigfetch include output

--- .github/workflows/install_llcppsigfetch.sh
+++ .github/workflows/install_llcppsigfetch.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 llgo install ./chore/_xtool/llcppsymg
+
+git clone -b llcppsigfetch/stdinclude https://github.com/luoliwoshang/llgo.git llgo_temp
+cd llgo_temp
+
 llgo install ./chore/_xtool/llcppsigfetch
+cd ..
+rm -rf llgo_temp
 
