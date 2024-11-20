16cf78f01c2deaaf8233afb06d8dea9b9848a35d
luoliwoshang <2643523683@qq.com>
2024-11-20 14:26:45 +0800

    llcppsigfetch:remove testdata

--- chore/_xtool/llcppsigfetch/parse/cvt_test/preprocess_test/testmarcoexpand/def.h
+++
@@ -1,4 +0,0 @@
-#define __FSID_T_TYPE                                                                                                  \
-    struct {                                                                                                           \
-        int __val[2];                                                                                                  \
-    }
