34cb3ed80e460880222f1e80e0991313b4bc857b
luoliwoshang <2643523683@qq.com>
2024-11-26 14:19:09 +0800

    ci:with upstream

--- .github/workflows/install_llcppg_depend.sh
+++ .github/workflows/install_llcppg_depend.sh
@@ -2,5 +2,11 @@
 set -e
 
 llgo install ./chore/_xtool/llcppsymg
+
+git clone -b llcppsigfetch/stdinclude https://github.com/luoliwoshang/llgo.git llgo_temp
+cd llgo_temp
 llgo install ./chore/_xtool/llcppsigfetch
+cd ..
+rm -rf llgo_temp
+
 go install ./chore/gogensig
