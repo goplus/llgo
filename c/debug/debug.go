7b6b8b0eeb84fb1fc9720fbb05e1c2b13600e0c1
visualfc <visualfc@gmail.com>
2024-11-29 11:27:56 +0800

    c/debug: StackTrace

--- c/debug/debug.go
+++ c/debug/debug.go
@@ -23,3 +23,23 @@ func Address() unsafe.Pointer
 
 //go:linkname Addrinfo C.llgo_addrinfo
 func Addrinfo(addr unsafe.Pointer, info *Info) c.Int
+
+//go:linkname stacktrace C.llgo_stacktrace
+func stacktrace(skip c.Int, ctx unsafe.Pointer, fn func(ctx, pc, offset, sp unsafe.Pointer, name *c.Char) c.Int)
+
+type Frame struct {
+	PC     uintptr
+	Offset uintptr
+	SP     unsafe.Pointer
+	Name   string
+}
+
+func StackTrace(skip int, fn func(fr *Frame) bool) {
+	stacktrace(c.Int(1+skip), unsafe.Pointer(&fn), func(ctx, pc, offset, sp unsafe.Pointer, name *c.Char) c.Int {
+		fn := *(*func(fr *Frame) bool)(ctx)
+		if !fn(&Frame{uintptr(pc), uintptr(offset), sp, c.GoString(name)}) {
+			return 0
+		}
+		return 1
+	})
+}
