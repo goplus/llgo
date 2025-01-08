ec744f7385e8d13e030d7d5d32dc78f20d7a4d90
Li Jie <cpunion@gmail.com>
2025-01-08 16:45:01 +0800

    chore: move compiler/chore/_xtool to _xtool

--- doc/_readme/scripts/install_llgo.sh
+++ doc/_readme/scripts/install_llgo.sh
@@ -3,6 +3,7 @@ git clone https://github.com/goplus/llgo.git
 cd llgo/compiler
 go install -v ./cmd/...
 go install -v ./chore/...  # compile all tools except pydump
-cd chore/_xtool
+export LLGO_ROOT=$PWD/..
+cd ../_xtool
 llgo install ./...   # compile pydump
 go install github.com/goplus/hdq/chore/pysigfetch@v0.8.1  # compile pysigfetch
