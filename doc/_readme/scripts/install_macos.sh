058f74c12c9131aaf90057e6d8a5b3db7dbb15e8
Li Jie <cpunion@gmail.com>
2025-02-13 18:19:34 +0800

    deps: add libuv into system deps

--- doc/_readme/scripts/install_macos.sh
+++ doc/_readme/scripts/install_macos.sh
@@ -1,6 +1,6 @@
 # shellcheck disable=all
 brew update
-brew install llvm@18 bdw-gc openssl cjson libffi pkg-config
+brew install llvm@18 bdw-gc openssl cjson libffi libuv pkg-config
 brew install python@3.12 # optional
 brew link --force libffi
 go install -v github.com/goplus/llgo/cmd/llgo@latest
