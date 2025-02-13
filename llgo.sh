ba46181365fae57d2ee35fee394f445938a2d45a
Li Jie <cpunion@gmail.com>
2025-02-13 13:36:53 +0800

    add llgo.sh for ease development

---
+++ llgo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+WORKDIR=''
+WORKDIR=$(pwd)
+LLGO_ROOT=''
+LLGO_ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
+export LLGO_ROOT
+cd $LLGO_ROOT/compiler
+go install ./cmd/llgo
+cd $WORKDIR
+llgo "$@"
