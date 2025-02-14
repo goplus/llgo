4e34ce74701f8bc86884c34c95cee71b5cf1fe6e
Li Jie <cpunion@gmail.com>
2025-02-14 19:25:06 +0800

    runtime: fix incorrect function signature

--- runtime/internal/lib/internal/syscall/unix/fcntl_unix.go
+++ runtime/internal/lib/internal/syscall/unix/fcntl_unix.go
@@ -11,11 +11,19 @@ const (
 	LLGoFiles   = "_unix/fcntl_unix.c"
 )
 
-//go:linkname fcntl C.llgo_fcntl2
-func fcntl(fd int32, cmd int32, arg int32) (int32, int32)
+//go:linkname llgo_fcntl2 C.llgo_fcntl2
+func llgo_fcntl2(fd int32, cmd int32, arg int32) (int32, int32)
 
 func Fcntl(fd int, cmd int, arg int) (int, error) {
-	val, errno := fcntl(int32(fd), int32(cmd), int32(arg))
+	val, errno := llgo_fcntl2(int32(fd), int32(cmd), int32(arg))
+	if val == -1 {
+		return int(val), syscall.Errno(errno)
+	}
+	return int(val), nil
+}
+
+func fcntl(fd int, cmd int, arg int) (int, error) {
+	val, errno := llgo_fcntl2(int32(fd), int32(cmd), int32(arg))
 	if val == -1 {
 		return int(val), syscall.Errno(errno)
 	}
