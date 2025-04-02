671ebe8478c87b0b0d8d562b01c69eefb848af10
xushiwei <x@goplus.org>
2025-04-03 00:01:35 +0800

    x

--- doc/_readme/scripts/install_macos.sh
+++ doc/_readme/scripts/install_macos.sh
@@ -3,4 +3,5 @@ brew update
 brew install llvm@18 bdw-gc openssl cjson libffi libuv pkg-config
 brew install python@3.12 # optional
 brew link --force libffi
-curl https://raw.githubusercontent.com/goplus/llgo/refs/heads/main/install.sh | bash
+#curl https://raw.githubusercontent.com/goplus/llgo/refs/heads/main/install.sh | bash
+cat ../../../install.sh | bash
