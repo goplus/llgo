a340b6ee01a804d1743ff276b3df12f2c9f43e10
luoliwoshang <2643523683@qq.com>
2025-07-15 14:11:53 +0800

    rm unuse code

--- xtool/env/llvm/llvm_config_byollvm.go
+++ xtool/env/llvm/llvm_config_byollvm.go
@@ -21,12 +21,6 @@
 
 package llvm
 
-import "fmt"
-
 // ldLLVMConfigBin is the path to the llvm-config binary. It shoud be set via
 // -ldflags when building the package.
 var ldLLVMConfigBin string
-
-func init() {
-	fmt.Println("Using custom LLVM configuration:", ldLLVMConfigBin)
-}
