96c7afcf3a4163ee6fbc3d884dc09917e1b22b3d
visualfc <visualfc@gmail.com>
2025-08-21 17:00:00 +0800

    build: dev support checkLinkArgs/CheckLLFiles/GenLLFiles

--- cmd/internal/flags/flags.go
+++ cmd/internal/flags/flags.go
@@ -3,6 +3,7 @@ package flags
 import (
 	"flag"
 
+	"github.com/goplus/llgo/internal/build"
 	"github.com/goplus/llgo/internal/buildenv"
 )
 
@@ -17,6 +18,9 @@ var BuildEnv string
 var Tags string
 var Target string
 var AbiMode int
+var CheckLinkArgs bool
+var CheckLLFiles bool
+var GenLLFiles bool
 
 func AddBuildFlags(fs *flag.FlagSet) {
 	fs.BoolVar(&Verbose, "v", false, "Verbose mode")
@@ -25,8 +29,9 @@ func AddBuildFlags(fs *flag.FlagSet) {
 	fs.StringVar(&Target, "target", "", "Target platform (e.g., rp2040, wasi)")
 	if buildenv.Dev {
 		fs.IntVar(&AbiMode, "abi", 2, "ABI mode (default 2). 0 = none, 1 = cfunc, 2 = allfunc.")
-	} else {
-		AbiMode = 2
+		fs.BoolVar(&CheckLinkArgs, "check-linkargs", false, "check link args valid")
+		fs.BoolVar(&CheckLLFiles, "check-llfiles", false, "check .ll files valid")
+		fs.BoolVar(&GenLLFiles, "gen-llfiles", false, "generate .ll files for pkg export")
 	}
 }
 
@@ -34,4 +39,22 @@ var Gen bool
 
 func AddCmpTestFlags(fs *flag.FlagSet) {
 	fs.BoolVar(&Gen, "gen", false, "Generate llgo.expect file")
+}
+
+func UpdateConfig(conf *build.Config) {
+	conf.Tags = Tags
+	conf.Verbose = Verbose
+	conf.Target = Target
+	switch conf.Mode {
+	case build.ModeBuild:
+		conf.OutFile = OutputFile
+	case build.ModeCmpTest:
+		conf.GenExpect = Gen
+	}
+	if buildenv.Dev {
+		conf.AbiMode = build.AbiMode(AbiMode)
+		conf.CheckLinkArgs = CheckLinkArgs
+		conf.CheckLLFiles = CheckLLFiles
+		conf.GenLL = GenLLFiles
+	}
 }
