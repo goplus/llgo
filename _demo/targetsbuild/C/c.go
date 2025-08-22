ad8aa6ac1a5d573fa8267940aa0dc65a3569331c
Li Jie <cpunion@gmail.com>
2025-08-22 08:42:01 +0800

    add __dynamic_loader into targetsbuild for nintendo switch

--- _demo/targetsbuild/C/c.go
+++ _demo/targetsbuild/C/c.go
@@ -11,3 +11,11 @@ func Reset_Handler() {
 func XhandleInterrupt() {
 
 }
+
+type dyn64 struct {
+	// Fields for dynamic loader
+}
+
+// For nintendo switch
+func X__dynamic_loader(base uintptr, dyn *dyn64) {
+}
