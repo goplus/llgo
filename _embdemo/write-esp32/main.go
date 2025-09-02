977806affc3b2302d02f7496d16789222535d5a5
Haolan <hello@liuxi.ng>
2025-09-02 22:35:08 +0800

    test: add more libc test

--- _embdemo/write-esp32/main.go
+++ _embdemo/write-esp32/main.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	_ "unsafe"
+
 	"github.com/goplus/lib/c"
 )
 
 //go:linkname write C.write
-func write(fd c.Int, *c.Char, c.SizeT) int
+func write(c.Int, *c.Char, c.SizeT) int
 
 func main() {
 	buf := c.Malloc(6)
 	c.Memset(buf, 0, 6)
 	c.Strncpy((*c.Char)(buf), c.Str("abcde"), 5)
+
+	if c.Strcmp((*c.Char)(buf), c.Str("abcde")) == 0 {
+		write(1, c.Str("pass strcmp"), 11)
+	}
+
+	if byte(c.Index((*c.Char)(buf), 0)) == 'a' {
+		write(1, c.Str("pass index"), 10)
+	}
+
+	c.Memset(buf, c.Int('A'), 5)
+	if c.Strcmp((*c.Char)(buf), c.Str("AAAAA")) == 0 {
+		write(1, c.Str("pass memeset"), 11)
+	}
 
 	write(1, (*c.Char)(buf), 5)
 }
