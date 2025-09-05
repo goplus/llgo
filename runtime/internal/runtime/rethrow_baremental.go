0d02a7273edadf149ed7f3a5f133cdf7144d6c40
Haolan <hello@liuxi.ng>
2025-09-05 16:41:58 +0800

    fix: rethrow in baremental

--- runtime/internal/runtime/rethrow_baremental.go
+++ runtime/internal/runtime/rethrow_baremental.go
@@ -2,5 +2,12 @@
 
 package runtime
 
+import c "github.com/goplus/llgo/runtime/internal/clite"
+
 // Rethrow rethrows a panic.
-func Rethrow(link *Defer) {}
+func Rethrow(link *Defer) {
+	// in baremental environment, we cannot get debug data from pthread_getspecific
+	// most of baremental implement of pthread_getspecific returns empty result
+	c.Printf(c.Str("fatal error\n"))
+	c.Exit(2)
+}
