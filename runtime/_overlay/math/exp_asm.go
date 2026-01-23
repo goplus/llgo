69f97909b997b943a473c489e07d61353e4fb826
Li Jie <cpunion@gmail.com>
2026-01-23 11:31:39 +0800

    runtime: add math overlay and keep sync/atomic alt

---
+++ runtime/_overlay/math/exp_asm.go
@@ -0,0 +1,14 @@
+//go:build llgo && (amd64 || arm64)
+
+package math
+
+import _ "unsafe"
+
+const haveArchExp = true
+
+//go:linkname c_exp C.exp
+func c_exp(x float64) float64
+
+func archExp(x float64) float64 {
+	return c_exp(x)
+}
--- runtime/overlay.go
+++ runtime/overlay.go
@@ -7,11 +7,27 @@ import (
 //go:embed _overlay/net/textproto/textproto.go
 var net_textproto string
 
+//go:embed _overlay/math/exp_asm.go
+var math_exp_asm string
+
+//go:embed _overlay/math/exp2_asm.go
+var math_exp2_asm string
+
+//go:embed _overlay/math/exp_amd64.s
+var math_exp_amd64_s string
+
+//go:embed _overlay/math/exp_arm64.s
+var math_exp_arm64_s string
+
 //go:embed _overlay/internal/sync/runtime.go
 var internal_sync string
 
 var OverlayFiles = map[string]string{
 	"math/exp_amd64.go":          "package math;",
+	"math/exp_asm.go":            math_exp_asm,
+	"math/exp2_asm.go":           math_exp2_asm,
+	"math/exp_amd64.s":           math_exp_amd64_s,
+	"math/exp_arm64.s":           math_exp_arm64_s,
 	"net/textproto/textproto.go": net_textproto,
 	"internal/sync/runtime.go":   internal_sync,
 }
