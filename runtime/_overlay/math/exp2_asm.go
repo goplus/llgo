e69df24884dbfdaf8faf0ee0cf9d6b4e8ab25af1
Li Jie <cpunion@gmail.com>
2026-01-23 11:31:39 +0800

    runtime: add math overlay and keep sync/atomic alt

---
+++ runtime/_overlay/math/exp2_asm.go
@@ -0,0 +1,14 @@
+//go:build llgo && arm64
+
+package math
+
+import _ "unsafe"
+
+const haveArchExp2 = true
+
+//go:linkname c_exp2 C.exp2
+func c_exp2(x float64) float64
+
+func archExp2(x float64) float64 {
+	return c_exp2(x)
+}
