75221827bea357f32dc0560e8bf5e353f013432d
Li Jie <cpunion@gmail.com>
2026-02-08 00:47:38 +0800

    runtime/internal/lib/internal/cpu: use LLVM IR cpuid helper

--- runtime/internal/lib/internal/cpu/cpu_x86.go
+++ runtime/internal/lib/internal/cpu/cpu_x86.go
@@ -4,26 +4,24 @@ package cpu
 
 import (
 	_ "unsafe"
-
-	c "github.com/goplus/llgo/runtime/internal/clite"
 )
 
 const (
 	LLGoPackage = "link"
-	LLGoFiles   = "_wrap/cpu_x86.c"
+	LLGoFiles   = "_wrap/cpu_x86.ll"
 )
 
 //go:linkname c_getcpuid C.llgo_getcpuid
-func c_getcpuid(eaxArg, ecxArg uint32, eax, ebx, ecx, edx *c.Uint)
+func c_getcpuid(eaxArg, ecxArg uint32, eax, ebx, ecx, edx *uint32)
 
 func cpuid(eaxArg, ecxArg uint32) (eax, ebx, ecx, edx uint32) {
 	c_getcpuid(
-		c.Uint(eaxArg),
-		c.Uint(ecxArg),
-		(*c.Uint)(&eax),
-		(*c.Uint)(&ebx),
-		(*c.Uint)(&ecx),
-		(*c.Uint)(&edx),
+		eaxArg,
+		ecxArg,
+		&eax,
+		&ebx,
+		&ecx,
+		&edx,
 	)
 	return
 }
