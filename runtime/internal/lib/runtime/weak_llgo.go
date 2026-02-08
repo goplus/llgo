d6d587316d8f1419adaee0438f2b1e10badf8611
Li Jie <cpunion@gmail.com>
2026-02-09 07:52:51 +0800

    feat(runtime): unpatch weak on arm64 amd64 for abi0 abi1

---
+++ runtime/internal/lib/runtime/weak_llgo.go
@@ -0,0 +1,37 @@
+package runtime
+
+import (
+	"unsafe"
+
+	"github.com/goplus/llgo/runtime/internal/clite/sync/atomic"
+	llrt "github.com/goplus/llgo/runtime/internal/runtime"
+	_ "unsafe"
+)
+
+type weakHandle struct {
+	pfn unsafe.Pointer // func() unsafe.Pointer
+}
+
+//go:linkname weak_runtime_registerWeakPointer weak.runtime_registerWeakPointer
+func weak_runtime_registerWeakPointer(p unsafe.Pointer) unsafe.Pointer {
+	n := uintptr(p) ^ 0xffff // hide from simple escape analysis
+	fn := func() unsafe.Pointer {
+		return unsafe.Pointer(n ^ 0xffff)
+	}
+	wh := &weakHandle{
+		pfn: unsafe.Pointer(&fn),
+	}
+	llrt.AddCleanupPtr(p, func() {
+		atomic.Store(&wh.pfn, nil)
+	})
+	return unsafe.Pointer(wh)
+}
+
+//go:linkname weak_runtime_makeStrongFromWeak weak.runtime_makeStrongFromWeak
+func weak_runtime_makeStrongFromWeak(u unsafe.Pointer) unsafe.Pointer {
+	wh := (*weakHandle)(u)
+	if pfn := atomic.Load(&wh.pfn); pfn != nil {
+		return (*(*func() unsafe.Pointer)(pfn))()
+	}
+	return nil
+}
