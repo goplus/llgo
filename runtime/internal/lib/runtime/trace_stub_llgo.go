b3ad65e2954e0e24c3d7f3322d662b1707a85567
Li Jie <cpunion@gmail.com>
2026-02-10 17:04:24 +0800

    runtime/trace: add missing user event stubs for llgo

--- runtime/internal/lib/runtime/trace_stub_llgo.go
+++ runtime/internal/lib/runtime/trace_stub_llgo.go
@@ -10,3 +10,15 @@ func traceClockNow() uint64 { return 0 }
 
 //go:linkname runtime_readTrace runtime/trace.runtime_readTrace
 func runtime_readTrace() []byte { return nil }
+
+//go:linkname trace_userTaskCreate runtime/trace.userTaskCreate
+func trace_userTaskCreate(id, parentID uint64, taskType string) {}
+
+//go:linkname trace_userTaskEnd runtime/trace.userTaskEnd
+func trace_userTaskEnd(id uint64) {}
+
+//go:linkname trace_userRegion runtime/trace.userRegion
+func trace_userRegion(id, mode uint64, regionType string) {}
+
+//go:linkname trace_userLog runtime/trace.userLog
+func trace_userLog(id uint64, category, message string) {}
