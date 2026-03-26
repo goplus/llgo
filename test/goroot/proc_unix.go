3ce72bb7ce7eae77386d8765576010a259c7b7e2
Li Jie <cpunion@gmail.com>
2026-03-26 08:17:12 +0800

    test: add goroot runner baseline and sharded CI

---
+++ test/goroot/proc_unix.go
@@ -0,0 +1,19 @@
+//go:build unix
+
+package goroot
+
+import (
+	"os/exec"
+	"syscall"
+)
+
+func configureProcessGroup(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+}
+
+func killProcessTree(cmd *exec.Cmd) {
+	if cmd.Process == nil {
+		return
+	}
+	_ = syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
+}
