5eb336532cef97c38ec8f023e9d645dff6621ce6
Li Jie <cpunion@gmail.com>
2026-03-28 22:27:23 +0800

    runtime: drop sync atomic skip from source patch proto

--- runtime/internal/lib/sync/atomic/value.go
+++ runtime/internal/lib/sync/atomic/value.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// llgo:skip runtime_procPin runtime_procUnpin
-
 package atomic
 
 import (
