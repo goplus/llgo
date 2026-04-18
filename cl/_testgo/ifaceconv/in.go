dbe82ea83cc53bddb2952cd34454d26a008a1588
Li Jie <cpunion@gmail.com>
2026-04-18 16:15:19 +0800

    test: keep ifaceconv key IR checks

--- cl/_testgo/ifaceconv/in.go
+++ cl/_testgo/ifaceconv/in.go
@@ -58,12 +58,31 @@ func (C2) f() {}
 func (C2) g() {}
 
 // CHECK-LABEL: define void @"{{.*}}/cl/_testgo/ifaceconv.main"() {
+// CHECK: br i1 false, label %{{.*}}, label %{{.*}}
 // CHECK: call ptr @"{{.*}}/runtime/internal/runtime.IfaceType"(%"{{.*}}/runtime/internal/runtime.iface" zeroinitializer)
-// CHECK: icmp ne ptr {{.*}}, null
-// CHECK: call i1 @"{{.*}}/runtime/internal/runtime.Implements"
+// CHECK-NEXT: %{{[0-9]+}} = icmp ne ptr %{{[0-9]+}}, null
+// CHECK-NEXT: br i1 %{{[0-9]+}}, label %{{.*}}, label %{{.*}}
+// CHECK: call ptr @"{{.*}}/runtime/internal/runtime.IfaceType"(%"{{.*}}/runtime/internal/runtime.iface" zeroinitializer)
+// CHECK-NEXT: %{{[0-9]+}} = icmp ne ptr %{{[0-9]+}}, null
+// CHECK-NEXT: br i1 %{{[0-9]+}}, label %{{.*}}, label %{{.*}}
+// CHECK: call ptr @"{{.*}}/runtime/internal/runtime.NewItab"(ptr @"{{.*}}/cl/_testgo/ifaceconv.iface$brpgdLtIeRlPi8QUoTgPCXzlehUkncg7v9aITo-GsF4", ptr %{{[0-9]+}})
+// CHECK: call ptr @"{{.*}}/runtime/internal/runtime.NewItab"(ptr @"{{.*}}/cl/_testgo/ifaceconv.iface$brpgdLtIeRlPi8QUoTgPCXzlehUkncg7v9aITo-GsF4", ptr @"_llgo_{{.*}}/cl/_testgo/ifaceconv.C1")
+// CHECK: call i1 @"{{.*}}/runtime/internal/runtime.Implements"(ptr @"_llgo_{{.*}}/cl/_testgo/ifaceconv.I0", ptr %{{[0-9]+}})
+// CHECK: call ptr @"{{.*}}/runtime/internal/runtime.IfaceType"(%"{{.*}}/runtime/internal/runtime.iface" %{{[0-9]+}})
+// CHECK-NEXT: %{{[0-9]+}} = icmp ne ptr %{{[0-9]+}}, null
+// CHECK-NEXT: br i1 %{{[0-9]+}}, label %{{.*}}, label %{{.*}}
+// CHECK: call i1 @"{{.*}}/runtime/internal/runtime.Implements"(ptr @"_llgo_{{.*}}/cl/_testgo/ifaceconv.I2", ptr %{{[0-9]+}})
+// CHECK: call ptr @"{{.*}}/runtime/internal/runtime.NewItab"(ptr @"{{.*}}/cl/_testgo/ifaceconv.iface$brpgdLtIeRlPi8QUoTgPCXzlehUkncg7v9aITo-GsF4", ptr @"_llgo_{{.*}}/cl/_testgo/ifaceconv.C2")
+// CHECK: call i1 @"{{.*}}/runtime/internal/runtime.Implements"(ptr @"_llgo_{{.*}}/cl/_testgo/ifaceconv.I0", ptr %{{[0-9]+}})
+// CHECK: call ptr @"{{.*}}/runtime/internal/runtime.IfaceType"(%"{{.*}}/runtime/internal/runtime.iface" %{{[0-9]+}})
+// CHECK-NEXT: %{{[0-9]+}} = icmp ne ptr %{{[0-9]+}}, null
+// CHECK-NEXT: br i1 %{{[0-9]+}}, label %{{.*}}, label %{{.*}}
+// CHECK: call i1 @"{{.*}}/runtime/internal/runtime.Implements"(ptr @"_llgo_{{.*}}/cl/_testgo/ifaceconv.I2", ptr %{{[0-9]+}})
+// CHECK: call i1 @"{{.*}}/runtime/internal/runtime.EfaceEqual"
 // CHECK: call i1 @"{{.*}}/runtime/internal/runtime.EfaceEqual"
 // CHECK: call void @"{{.*}}/runtime/internal/runtime.PrintString"
-// CHECK: ret void
+// CHECK-NEXT: call void @"{{.*}}/runtime/internal/runtime.PrintByte"
+// CHECK-NEXT: ret void
 func main() {
 	var i0 I0
 	var i1 I1
