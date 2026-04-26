0647f3cfccc6ed7e3f6a746eba26ae47029aa7ec
Li Jie <cpunion@gmail.com>
2026-04-26 16:38:49 +0800

    compact compiler-rt file lists for faster Go builds
    Result: {"status":"keep","go_build_ms":10301.1,"go_build_min_ms":10234.3,"go_build_max_ms":10470.5,"runs":3,"timed_compile_invocations":0,"slowest_pkg_compile_ms":0,"filepath_join_count":3,"join_file_list_uses":6,"best_p":0,"p_values_tested":0,"ideas_updated":1}

--- internal/crosscompile/compile/rtlib/compiler_rt.go
+++ internal/crosscompile/compile/rtlib/compiler_rt.go
@@ -8,93 +8,96 @@ import (
 	"github.com/goplus/llgo/internal/crosscompile/compile"
 )
 
+func joinFileList(root, files string) []string {
+	items := strings.Fields(files)
+	paths := make([]string, 0, len(items))
+	for _, item := range items {
+		paths = append(paths, filepath.Join(root, filepath.FromSlash(item)))
+	}
+	return paths
+}
+
 func platformSpecifiedFiles(builtinsDir, target string) []string {
 	switch {
 	case strings.Contains(target, "riscv32"):
-		files := []string{
-			filepath.Join(builtinsDir, "riscv", "mulsi3.S"),
-			filepath.Join(builtinsDir, "riscv", "fp_mode.c"),
-			filepath.Join(builtinsDir, "riscv", "save.S"),
-			filepath.Join(builtinsDir, "riscv", "restore.S"),
-		}
+		files := joinFileList(builtinsDir, `
+riscv/mulsi3.S
+riscv/fp_mode.c
+riscv/save.S
+riscv/restore.S
+		`)
 		// Only add atomic.c for non-ESP targets (ESP doesn't support A extension)
 		if target != "riscv32-esp-elf" {
 			files = append(files, filepath.Join(builtinsDir, "atomic.c"))
 		}
 		return files
 	case strings.Contains(target, "riscv64"):
-		return []string{
-			filepath.Join(builtinsDir, "addtf3.c"),
-			filepath.Join(builtinsDir, "comparetf2.c"),
-			filepath.Join(builtinsDir, "divtc3.c"),
-			filepath.Join(builtinsDir, "divtf3.c"),
-			filepath.Join(builtinsDir, "extenddftf2.c"),
-			filepath.Join(builtinsDir, "extendhftf2.c"),
-			filepath.Join(builtinsDir, "extendsftf2.c"),
-			filepath.Join(builtinsDir, "fixtfdi.c"),
-			filepath.Join(builtinsDir, "fixtfsi.c"),
-			filepath.Join(builtinsDir, "fixtfti.c"),
-			filepath.Join(builtinsDir, "fixunstfdi.c"),
-			filepath.Join(builtinsDir, "fixunstfsi.c"),
-			filepath.Join(builtinsDir, "fixunstfti.c"),
-			filepath.Join(builtinsDir, "floatditf.c"),
-			filepath.Join(builtinsDir, "floatsitf.c"),
-			filepath.Join(builtinsDir, "floattitf.c"),
-			filepath.Join(builtinsDir, "floatunditf.c"),
-			filepath.Join(builtinsDir, "floatunsitf.c"),
-			filepath.Join(builtinsDir, "floatuntitf.c"),
-			filepath.Join(builtinsDir, "multc3.c"),
-			filepath.Join(builtinsDir, "multf3.c"),
-			filepath.Join(builtinsDir, "powitf2.c"),
-			filepath.Join(builtinsDir, "subtf3.c"),
-			filepath.Join(builtinsDir, "trunctfdf2.c"),
-			filepath.Join(builtinsDir, "trunctfhf2.c"),
-			filepath.Join(builtinsDir, "trunctfsf2.c"),
-			filepath.Join(builtinsDir, "atomic.c"),
-		}
+		return joinFileList(builtinsDir, `
+addtf3.c
+comparetf2.c
+divtc3.c
+divtf3.c
+extenddftf2.c
+extendhftf2.c
+extendsftf2.c
+fixtfdi.c
+fixtfsi.c
+fixtfti.c
+fixunstfdi.c
+fixunstfsi.c
+fixunstfti.c
+floatditf.c
+floatsitf.c
+floattitf.c
+floatunditf.c
+floatunsitf.c
+floatuntitf.c
+multc3.c
+multf3.c
+powitf2.c
+subtf3.c
+trunctfdf2.c
+trunctfhf2.c
+trunctfsf2.c
+atomic.c
+		`)
 	case strings.Contains(target, "arm"):
-		return []string{
-			filepath.Join(builtinsDir, "arm", "aeabi_cdcmp.S"),
-			filepath.Join(builtinsDir, "arm", "aeabi_cdcmpeq_check_nan.c"),
-			filepath.Join(builtinsDir, "arm", "aeabi_cfcmp.S"),
-			filepath.Join(builtinsDir, "arm", "aeabi_cfcmpeq_check_nan.c"),
-			filepath.Join(builtinsDir, "arm", "aeabi_dcmp.S"),
-			filepath.Join(builtinsDir, "arm", "aeabi_div0.c"),
-			filepath.Join(builtinsDir, "arm", "aeabi_drsub.c"),
-			filepath.Join(builtinsDir, "arm", "aeabi_fcmp.S"),
-			filepath.Join(builtinsDir, "arm", "aeabi_frsub.c"),
-			filepath.Join(builtinsDir, "arm", "aeabi_idivmod.S"),
-			filepath.Join(builtinsDir, "arm", "aeabi_ldivmod.S"),
-			filepath.Join(builtinsDir, "arm", "aeabi_memcmp.S"),
-			filepath.Join(builtinsDir, "arm", "aeabi_memcpy.S"),
-			filepath.Join(builtinsDir, "arm", "aeabi_memmove.S"),
-			filepath.Join(builtinsDir, "arm", "aeabi_memset.S"),
-			filepath.Join(builtinsDir, "arm", "aeabi_uidivmod.S"),
-			filepath.Join(builtinsDir, "arm", "aeabi_uldivmod.S"),
-
-			// These two are not technically EABI builtins but are used by them and only
-			// seem to be used on ARM. LLVM seems to use __divsi3 and __modsi3 on most
-			// other architectures.
-			// Most importantly, they have a different calling convention on AVR so
-			// should not be used on AVR.
-			filepath.Join(builtinsDir, "divmodsi4.c"),
-			filepath.Join(builtinsDir, "udivmodsi4.c"),
-		}
+		return joinFileList(builtinsDir, `
+arm/aeabi_cdcmp.S
+arm/aeabi_cdcmpeq_check_nan.c
+arm/aeabi_cfcmp.S
+arm/aeabi_cfcmpeq_check_nan.c
+arm/aeabi_dcmp.S
+arm/aeabi_div0.c
+arm/aeabi_drsub.c
+arm/aeabi_fcmp.S
+arm/aeabi_frsub.c
+arm/aeabi_idivmod.S
+arm/aeabi_ldivmod.S
+arm/aeabi_memcmp.S
+arm/aeabi_memcpy.S
+arm/aeabi_memmove.S
+arm/aeabi_memset.S
+arm/aeabi_uidivmod.S
+arm/aeabi_uldivmod.S
+divmodsi4.c
+udivmodsi4.c
+		`)
 	case strings.Contains(target, "avr"):
-		return []string{
-			filepath.Join(builtinsDir, "avr", "divmodhi4.S"),
-			filepath.Join(builtinsDir, "avr", "divmodqi4.S"),
-			filepath.Join(builtinsDir, "avr", "mulhi3.S"),
-			filepath.Join(builtinsDir, "avr", "mulqi3.S"),
-			filepath.Join(builtinsDir, "avr", "udivmodhi4.S"),
-			filepath.Join(builtinsDir, "avr", "udivmodqi4.S"),
-		}
+		return joinFileList(builtinsDir, `
+avr/divmodhi4.S
+avr/divmodqi4.S
+avr/mulhi3.S
+avr/mulqi3.S
+avr/udivmodhi4.S
+avr/udivmodqi4.S
+		`)
 
 	case target == "xtensa":
-		return []string{
-			filepath.Join(builtinsDir, "xtensa", "ieee754_sqrtf.S"),
-			filepath.Join(builtinsDir, "atomic.c"),
-		}
+		return joinFileList(builtinsDir, `
+xtensa/ieee754_sqrtf.S
+atomic.c
+		`)
 	}
 	return nil
 }
@@ -118,155 +121,155 @@ func GetCompilerRTCompileConfig(baseDir, target string) compile.CompileConfig {
 		Groups: []compile.CompileGroup{
 			{
 				OutputFileName: fmt.Sprintf("libclang_builtins-%s.a", target),
-				Files: withPlatformSpecifiedFiles(baseDir, target, []string{
-					filepath.Join(baseDir, "lib", "builtins", "absvdi2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "absvsi2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "absvti2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "adddf3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "addsf3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "addvdi3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "addvsi3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "addvti3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "apple_versioning.c"),
-					filepath.Join(baseDir, "lib", "builtins", "ashldi3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "ashlti3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "ashrdi3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "ashrti3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "bswapdi2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "bswapsi2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "clzdi2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "clzsi2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "clzti2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "cmpdi2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "cmpti2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "comparedf2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "comparesf2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "ctzdi2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "ctzsi2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "ctzti2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "divdc3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "divdf3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "divdi3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "divmoddi4.c"),
-					filepath.Join(baseDir, "lib", "builtins", "divmodsi4.c"),
-					filepath.Join(baseDir, "lib", "builtins", "divmodti4.c"),
-					filepath.Join(baseDir, "lib", "builtins", "divsc3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "divsf3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "divsi3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "divti3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "extendsfdf2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "extendhfsf2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "ffsdi2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "ffssi2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "ffsti2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "fixdfdi.c"),
-					filepath.Join(baseDir, "lib", "builtins", "fixdfsi.c"),
-					filepath.Join(baseDir, "lib", "builtins", "fixdfti.c"),
-					filepath.Join(baseDir, "lib", "builtins", "fixsfdi.c"),
-					filepath.Join(baseDir, "lib", "builtins", "fixsfsi.c"),
-					filepath.Join(baseDir, "lib", "builtins", "fixsfti.c"),
-					filepath.Join(baseDir, "lib", "builtins", "fixunsdfdi.c"),
-					filepath.Join(baseDir, "lib", "builtins", "fixunsdfsi.c"),
-					filepath.Join(baseDir, "lib", "builtins", "fixunsdfti.c"),
-					filepath.Join(baseDir, "lib", "builtins", "fixunssfdi.c"),
-					filepath.Join(baseDir, "lib", "builtins", "fixunssfsi.c"),
-					filepath.Join(baseDir, "lib", "builtins", "fixunssfti.c"),
-					filepath.Join(baseDir, "lib", "builtins", "floatdidf.c"),
-					filepath.Join(baseDir, "lib", "builtins", "floatdisf.c"),
-					filepath.Join(baseDir, "lib", "builtins", "floatsidf.c"),
-					filepath.Join(baseDir, "lib", "builtins", "floatsisf.c"),
-					filepath.Join(baseDir, "lib", "builtins", "floattidf.c"),
-					filepath.Join(baseDir, "lib", "builtins", "floattisf.c"),
-					filepath.Join(baseDir, "lib", "builtins", "floatundidf.c"),
-					filepath.Join(baseDir, "lib", "builtins", "floatundisf.c"),
-					filepath.Join(baseDir, "lib", "builtins", "floatunsidf.c"),
-					filepath.Join(baseDir, "lib", "builtins", "floatunsisf.c"),
-					filepath.Join(baseDir, "lib", "builtins", "floatuntidf.c"),
-					filepath.Join(baseDir, "lib", "builtins", "floatuntisf.c"),
-					filepath.Join(baseDir, "lib", "builtins", "fp_mode.c"),
-					filepath.Join(baseDir, "lib", "builtins", "int_util.c"),
-					filepath.Join(baseDir, "lib", "builtins", "lshrdi3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "lshrti3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "moddi3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "modsi3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "modti3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "muldc3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "muldf3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "muldi3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "mulodi4.c"),
-					filepath.Join(baseDir, "lib", "builtins", "mulosi4.c"),
-					filepath.Join(baseDir, "lib", "builtins", "muloti4.c"),
-					filepath.Join(baseDir, "lib", "builtins", "mulsc3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "mulsf3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "multi3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "mulvdi3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "mulvsi3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "mulvti3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "negdf2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "negdi2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "negsf2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "negti2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "negvdi2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "negvsi2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "negvti2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "os_version_check.c"),
-					filepath.Join(baseDir, "lib", "builtins", "paritydi2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "paritysi2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "parityti2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "popcountdi2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "popcountsi2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "popcountti2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "powidf2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "powisf2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "subdf3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "subsf3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "subvdi3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "subvsi3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "subvti3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "trampoline_setup.c"),
-					filepath.Join(baseDir, "lib", "builtins", "truncdfhf2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "truncdfsf2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "truncsfhf2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "ucmpdi2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "ucmpti2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "udivdi3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "udivmoddi4.c"),
-					filepath.Join(baseDir, "lib", "builtins", "udivmodsi4.c"),
-					filepath.Join(baseDir, "lib", "builtins", "udivmodti4.c"),
-					filepath.Join(baseDir, "lib", "builtins", "udivsi3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "udivti3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "umoddi3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "umodsi3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "umodti3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "gcc_personality_v0.c"),
-					filepath.Join(baseDir, "lib", "builtins", "clear_cache.c"),
-					filepath.Join(baseDir, "lib", "builtins", "addtf3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "comparetf2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "divtc3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "divtf3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "extenddftf2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "extendhftf2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "extendsftf2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "fixtfdi.c"),
-					filepath.Join(baseDir, "lib", "builtins", "fixtfsi.c"),
-					filepath.Join(baseDir, "lib", "builtins", "fixtfti.c"),
-					filepath.Join(baseDir, "lib", "builtins", "fixunstfdi.c"),
-					filepath.Join(baseDir, "lib", "builtins", "fixunstfsi.c"),
-					filepath.Join(baseDir, "lib", "builtins", "fixunstfti.c"),
-					filepath.Join(baseDir, "lib", "builtins", "floatditf.c"),
-					filepath.Join(baseDir, "lib", "builtins", "floatsitf.c"),
-					filepath.Join(baseDir, "lib", "builtins", "floattitf.c"),
-					filepath.Join(baseDir, "lib", "builtins", "floatunditf.c"),
-					filepath.Join(baseDir, "lib", "builtins", "floatunsitf.c"),
-					filepath.Join(baseDir, "lib", "builtins", "floatuntitf.c"),
-					filepath.Join(baseDir, "lib", "builtins", "multc3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "multf3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "powitf2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "subtf3.c"),
-					filepath.Join(baseDir, "lib", "builtins", "trunctfdf2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "trunctfhf2.c"),
-					filepath.Join(baseDir, "lib", "builtins", "trunctfsf2.c"),
-				}),
+				Files: withPlatformSpecifiedFiles(baseDir, target, joinFileList(baseDir, `
+lib/builtins/absvdi2.c
+lib/builtins/absvsi2.c
+lib/builtins/absvti2.c
+lib/builtins/adddf3.c
+lib/builtins/addsf3.c
+lib/builtins/addvdi3.c
+lib/builtins/addvsi3.c
+lib/builtins/addvti3.c
+lib/builtins/apple_versioning.c
+lib/builtins/ashldi3.c
+lib/builtins/ashlti3.c
+lib/builtins/ashrdi3.c
+lib/builtins/ashrti3.c
+lib/builtins/bswapdi2.c
+lib/builtins/bswapsi2.c
+lib/builtins/clzdi2.c
+lib/builtins/clzsi2.c
+lib/builtins/clzti2.c
+lib/builtins/cmpdi2.c
+lib/builtins/cmpti2.c
+lib/builtins/comparedf2.c
+lib/builtins/comparesf2.c
+lib/builtins/ctzdi2.c
+lib/builtins/ctzsi2.c
+lib/builtins/ctzti2.c
+lib/builtins/divdc3.c
+lib/builtins/divdf3.c
+lib/builtins/divdi3.c
+lib/builtins/divmoddi4.c
+lib/builtins/divmodsi4.c
+lib/builtins/divmodti4.c
+lib/builtins/divsc3.c
+lib/builtins/divsf3.c
+lib/builtins/divsi3.c
+lib/builtins/divti3.c
+lib/builtins/extendsfdf2.c
+lib/builtins/extendhfsf2.c
+lib/builtins/ffsdi2.c
+lib/builtins/ffssi2.c
+lib/builtins/ffsti2.c
+lib/builtins/fixdfdi.c
+lib/builtins/fixdfsi.c
+lib/builtins/fixdfti.c
+lib/builtins/fixsfdi.c
+lib/builtins/fixsfsi.c
+lib/builtins/fixsfti.c
+lib/builtins/fixunsdfdi.c
+lib/builtins/fixunsdfsi.c
+lib/builtins/fixunsdfti.c
+lib/builtins/fixunssfdi.c
+lib/builtins/fixunssfsi.c
+lib/builtins/fixunssfti.c
+lib/builtins/floatdidf.c
+lib/builtins/floatdisf.c
+lib/builtins/floatsidf.c
+lib/builtins/floatsisf.c
+lib/builtins/floattidf.c
+lib/builtins/floattisf.c
+lib/builtins/floatundidf.c
+lib/builtins/floatundisf.c
+lib/builtins/floatunsidf.c
+lib/builtins/floatunsisf.c
+lib/builtins/floatuntidf.c
+lib/builtins/floatuntisf.c
+lib/builtins/fp_mode.c
+lib/builtins/int_util.c
+lib/builtins/lshrdi3.c
+lib/builtins/lshrti3.c
+lib/builtins/moddi3.c
+lib/builtins/modsi3.c
+lib/builtins/modti3.c
+lib/builtins/muldc3.c
+lib/builtins/muldf3.c
+lib/builtins/muldi3.c
+lib/builtins/mulodi4.c
+lib/builtins/mulosi4.c
+lib/builtins/muloti4.c
+lib/builtins/mulsc3.c
+lib/builtins/mulsf3.c
+lib/builtins/multi3.c
+lib/builtins/mulvdi3.c
+lib/builtins/mulvsi3.c
+lib/builtins/mulvti3.c
+lib/builtins/negdf2.c
+lib/builtins/negdi2.c
+lib/builtins/negsf2.c
+lib/builtins/negti2.c
+lib/builtins/negvdi2.c
+lib/builtins/negvsi2.c
+lib/builtins/negvti2.c
+lib/builtins/os_version_check.c
+lib/builtins/paritydi2.c
+lib/builtins/paritysi2.c
+lib/builtins/parityti2.c
+lib/builtins/popcountdi2.c
+lib/builtins/popcountsi2.c
+lib/builtins/popcountti2.c
+lib/builtins/powidf2.c
+lib/builtins/powisf2.c
+lib/builtins/subdf3.c
+lib/builtins/subsf3.c
+lib/builtins/subvdi3.c
+lib/builtins/subvsi3.c
+lib/builtins/subvti3.c
+lib/builtins/trampoline_setup.c
+lib/builtins/truncdfhf2.c
+lib/builtins/truncdfsf2.c
+lib/builtins/truncsfhf2.c
+lib/builtins/ucmpdi2.c
+lib/builtins/ucmpti2.c
+lib/builtins/udivdi3.c
+lib/builtins/udivmoddi4.c
+lib/builtins/udivmodsi4.c
+lib/builtins/udivmodti4.c
+lib/builtins/udivsi3.c
+lib/builtins/udivti3.c
+lib/builtins/umoddi3.c
+lib/builtins/umodsi3.c
+lib/builtins/umodti3.c
+lib/builtins/gcc_personality_v0.c
+lib/builtins/clear_cache.c
+lib/builtins/addtf3.c
+lib/builtins/comparetf2.c
+lib/builtins/divtc3.c
+lib/builtins/divtf3.c
+lib/builtins/extenddftf2.c
+lib/builtins/extendhftf2.c
+lib/builtins/extendsftf2.c
+lib/builtins/fixtfdi.c
+lib/builtins/fixtfsi.c
+lib/builtins/fixtfti.c
+lib/builtins/fixunstfdi.c
+lib/builtins/fixunstfsi.c
+lib/builtins/fixunstfti.c
+lib/builtins/floatditf.c
+lib/builtins/floatsitf.c
+lib/builtins/floattitf.c
+lib/builtins/floatunditf.c
+lib/builtins/floatunsitf.c
+lib/builtins/floatuntitf.c
+lib/builtins/multc3.c
+lib/builtins/multf3.c
+lib/builtins/powitf2.c
+lib/builtins/subtf3.c
+lib/builtins/trunctfdf2.c
+lib/builtins/trunctfhf2.c
+lib/builtins/trunctfsf2.c
+				`)),
 				CFlags: []string{
 					"-DNDEBUG",
 					"-DVISIBILITY_HIDDEN",
