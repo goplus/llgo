a92a668a17f8afa76a631b9ed3ec7b81a98e6741
zhouguangyuan0718 <zhouguangyuan.xian@gmail.com>
2026-04-28 09:57:20 +0800

    Fix comment formatting in in.go

--- cl/_testlibgo/mapzero/in.go
+++ cl/_testlibgo/mapzero/in.go
@@ -10,7 +10,7 @@ func main() {
 		err := recover()
 		fmt.Println(err)
 	}()
-    // CHECK: call ptr @"github.com/goplus/llgo/runtime/internal/runtime.MapAccess1"(ptr @"map[_llgo_int]_llgo_int", ptr null, ptr {{%.*}})
+	// CHECK: call ptr @"github.com/goplus/llgo/runtime/internal/runtime.MapAccess1"(ptr @"map[_llgo_int]_llgo_int", ptr null, ptr {{%.*}})
 	m := [0]map[int]int{}[a][0]
 	print(m)
 }
