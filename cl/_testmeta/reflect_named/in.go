1d1bb0f73ef32c8d4954c3f312d621b2d1d9948d
luoliwoshang <2643523683@qq.com>
2026-05-03 21:16:01 +0800

    ssa: keep raw MethodByName names in metadata

--- cl/_testmeta/reflect_named/in.go
+++ cl/_testmeta/reflect_named/in.go
@@ -5,7 +5,9 @@ import "reflect"
 type T struct{}
 
 func (T) M() {}
+func (T) m() {}
 
 func main() {
 	_, _ = reflect.TypeOf(T{}).MethodByName("M")
+	_, _ = reflect.TypeOf(T{}).MethodByName("m")
 }
--- ssa/metadata.go
+++ ssa/metadata.go
@@ -1,17 +1,12 @@
 package ssa
 
 import (
-	"go/token"
-
 	"github.com/goplus/llgo/internal/semmeta"
 )
 
 // EmitUseNamedMethod records a MethodByName-like exact method-name demand for
 // later whole-program deadcode analysis.
 func (p Package) EmitUseNamedMethod(owner, name string) {
-	if !token.IsExported(name) && p.Path() != "" {
-		name = p.Path() + "." + name
-	}
 	p.semMetaEmitter.AddUseNamedMethod(semmeta.Symbol(owner), name)
 }
 
